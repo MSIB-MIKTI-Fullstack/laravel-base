a3dd2162819268bb3da912d6412a876fb8e96936
indahsalim <indahsalim4@gmail.com>
2024-03-20 13:48:35 +0700

    addtext

---
+++ app/Http/Controllers/halamancontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class halamancontroller extends Controller
+{
+    public function index()
+    {
+        return '<h1>Ini Halaman Indah</h1>';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\halamancontroller;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,22 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+// 127.0.0.1:8000/ ==> view welcome
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/belajar', function () {
+    return '<h1>Saya Indah Oktavia Belajar Routing</h1>';
+});
+
+Route::get('/halaman', function () {
+    return view('halaman');
+});
+
+Route::get('/halamanku', function () {
+    return view('halaman' , ['judul' => 'Mari Belajar Routing Di Laravel']);
+});
+ 
+Route::get('/halaman', [halamanController::class, 'index']);
