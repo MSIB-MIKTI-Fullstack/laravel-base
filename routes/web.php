4bc7ae084d2532eb382c3a158d6fed03c8421b2b
zelviannisaputri <zelviannisa@gmail.com>
2024-03-20 11:31:23 +0700

    Belajar Route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContohController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contoh', [App\Http\Controllers\ContohController::class, 'contoh']);
