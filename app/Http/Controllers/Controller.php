f4cf4cfa4a5cebcf9ecaac8092bb8ffd89d7a21c
rizkirmr <rizkilis1329@gmail.com>
2024-03-20 16:29:06 +0700

    add:HomeController.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,12 +1,15 @@
 <?php
-
 namespace App\Http\Controllers;
-
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
-
-class Controller extends BaseController
+use Illuminate\Http\Request;
+class HomeController extends Controller
 {
-    use AuthorizesRequests, ValidatesRequests;
+    public function index()
+    {
+        return view('home');
+    }
+    public function dashboard()
+    {
+        $teks = "Passing data dari controller";
+        return view('dashboard', compact('teks'));
+    }
 }
