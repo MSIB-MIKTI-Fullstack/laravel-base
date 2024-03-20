c0a378c1a84cca5e54022ce98d114f845c856a26
linggaralfin <linggaralfinnursafitri@gmail.com>
2024-03-20 01:50:26 -0400

    menambahkan file blade.php

--- routes/routing.blade.php
+++ routes/routing.blade.php
@@ -14,16 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 //127.0.0.1:8000/siswa ==>  <h1>SAYA SISWA</h1>
-// Route::get('/siswa', function () {
-//     return "<h1>SAYA SISWA</h1>";
-// });
+Route::get('/siswa', function () {
+    return "<h1>SAYA SISWA</h1>";
+});
 
-// //127.0.0.1:8000/siswa/id ==>  <h1>SAYA SISWA dengan id</h1>
-// Route::get('/siswa/{id}', function ($id) {
-//     return "<h1>SAYA SISWA dengan ID $id</h1>";
-// })-> where('id', '[0-9]+');
+//127.0.0.1:8000/siswa/id ==>  <h1>SAYA SISWA dengan id</h1>
+Route::get('/siswa/{id}', function ($id) {
+    return "<h1>SAYA SISWA dengan ID $id</h1>";
+})-> where('id', '[0-9]+');
 
-// //127.0.0.1:8000/siswa/id/nama ==>  <h1>SAYA SISWA dengan id dan nama</h1>
-// Route::get('/siswa/{id}/{nama}', function ($id, $nama) {
-//     return "<h1>SAYA SISWA dengan ID $id dan Nama $nama</h1>";
-// })-> where(['id' => '[0-9]+', 'nama' => '[A-Za-z]+']);
+//127.0.0.1:8000/siswa/id/nama ==>  <h1>SAYA SISWA dengan id dan nama</h1>
+Route::get('/siswa/{id}/{nama}', function ($id, $nama) {
+    return "<h1>SAYA SISWA dengan ID $id dan Nama $nama</h1>";
+})-> where(['id' => '[0-9]+', 'nama' => '[A-Za-z]+']);
+
