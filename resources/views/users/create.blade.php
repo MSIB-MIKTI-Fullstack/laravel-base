0b5952c67c7cf31d7e3b4fdad50a44bded829bd7
MohammadIndraA <akunpertamasaya5@gmail.com>
2024-03-27 21:19:20 +0700

    add: belajar view component

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.admin.app')
+{{-- @extends('layouts.admin.app')
 @section('title_page')
     Halaman Create User
 @endsection
@@ -11,4 +11,17 @@
 @endsection
 @section('content')
     Ini Halaman Create User
-@endsection
+@endsection --}}
+<x-admin-layout>
+    <x-slot:title>
+        Create User
+    </x-slot:title>
+    <x-slot:breadcrum>
+        <li><a href="#" class="text-gray-500 dark:text-slate-400">User</a></li>
+        <li><span class="text-gray-500 dark:text-slate-400 mx-2">/</span></li>
+        <li class="text-gray-500 dark:text-slate-400">Dashboard</li>
+        <li><span class="text-gray-500 dark:text-slate-400 mx-2">/</span></li>
+        <li class="text-primary-500 hover:text-primary-600 dark:text-primary-400">User Create</li>
+    </x-slot:breadcrum>
+    Ini Content Create Table User
+</x-admin-layout>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.admin.app')
+{{-- @extends('layouts.admin.app')
 @section('title_page')
     Halaman Daftar User
 @endsection
@@ -11,4 +11,17 @@
 @endsection
 @section('content')
     Ini Halaman Dafat User
-@endsection
+@endsection --}}
+
+<x-admin-layout>
+    <x-slot:title>Home User Table</x-slot:title>
+    <x-slot:breadcrum>
+        <li><a href="#" class="text-gray-500 dark:text-slate-400">User</a></li>
+        <li><span class="text-gray-500 dark:text-slate-400 mx-2">/</span></li>
+        <li class="text-gray-500 dark:text-slate-400">Dashboard</li>
+        <li><span class="text-gray-500 dark:text-slate-400 mx-2">/</span></li>
+        <li class="text-primary-500 hover:text-primary-600 dark:text-primary-400">User Table</li>
+    </x-slot:breadcrum>
+
+    Ini Content
+</x-admin-layout>
