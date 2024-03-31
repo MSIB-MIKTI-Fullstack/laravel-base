1ee38b89cfd87f078e732742b18789d8429e5726
rrnaldi <renaldi240402@gmail.com>
2024-03-31 11:22:59 +0700

    belajar layout

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,6 @@ class HomeController extends Controller
 {
     public function testing()
     {
-        return view('users.index');
+        return view('layout.app');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Home</title>
-</head>
+@section('title', 'Users')
 
-<body>
-    <h1>Ini Adalah Halaman Home</h1>
-</body>
-
-</html>
+@section('content')
+<div class="ltr:flex flex-1 rtl:flex-row-reverse">
+    <div class="page-wrapper relative ltr:ml-auto rtl:mr-auto rtl:ml-0 w-[calc(100%-260px)] px-4 pt-[64px] duration-300">
+        <div class="xl:w-full">
+            <div class="flex flex-wrap">
+                <div class="flex items-center py-4 w-full">
+                    <div class="w-full">
+                        <div class="">
+                            @include('partials.page_header')
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
