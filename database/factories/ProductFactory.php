1080d8bf86c93384d8c989d5d868c9bbe77c9edc
rafasepti <rafasepti@gmail.com>
2024-04-03 18:05:25 +0700

    latihan model

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = fake()->text(25);
+        return [
+            'name' => $name,
+            'description' => fake()->text(255),
+            'image' => fake()->imageUrl(640, 640),
+            'price' => fake()->numberBetween(10000, 1000000),
+            'slug' => Str::slug($name)
+        ];
+    }
+}
