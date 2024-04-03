97efad6c4a57642d48902f44b00ab1e75de0745b
MohammadIndraA <akunpertamasaya5@gmail.com>
2024-04-03 17:50:59 +0700

    add: belajar model

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {   
+        $name = fake()->text(25);
+        return [
+            'name' => $name,
+            'description' => fake()->text(255),
+            'image' => fake()->imageUrl(640, 640),
+            'price' => fake()->numberBetween(10000, 100000),
+            'slug' => Str::slug($name)
+        ];
+    }
+}
