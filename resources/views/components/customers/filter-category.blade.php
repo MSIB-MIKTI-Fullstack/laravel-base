8aef425c1abbf5380da24e32f94fa404bfa1f78c
DimasPratama03 <112364293+DimasPratama03@users.noreply.github.com>
2024-04-06 17:08:25 +0700

    Add : Customers Layout

--- resources/views/components/customers/filter-category.blade.php
+++ resources/views/components/customers/filter-category.blade.php
@@ -1,3 +1,15 @@
 <div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
+    <h4 class="font-medium flex-1 self-center mb-3 text-base">Categories</h4>
+    <form action="">
+        @foreach ($categories as $item)
+            <a href="/beranda?category_id={{ $item->id }}">
+                <div
+                    class="px-4 py-1 lg:px-4 bg-transparent  text-brand text-base  transition hover:bg-brand-500/10 hover:text-brand-500 border border-slate-200 border-dashed font-medium w-full">
+                    <label class="custom-label inline-block dark:text-slate-300">
+                        <span class="ms-1">{{ $item->name }}</span>
+                    </label>
+                </div>
+            </a>
+        @endforeach
+    </form>
 </div>
