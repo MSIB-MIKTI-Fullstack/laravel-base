6b2dbd21ebe93b41f289ef5b1f22272ff9bb8c38
Aurel Izzety <aurelizzety@gmail.com>
2024-04-17 20:49:31 +0700

    add: pagination products

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use App\Models\User;
-use App\Service\MailService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
@@ -15,7 +13,7 @@ class HomeController extends Controller
         $products = Product::with(['product_category'])
             ->when($request->category_id != "", function ($q) use ($request) {
                 $q->where('product_category_id', $request->category_id);
-            })->get();
+            })->paginate(10);
 
         return view('customers.product', compact('products'));
     }
