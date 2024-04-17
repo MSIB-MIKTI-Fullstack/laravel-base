e3b733d9d5fa44ee487f23f3c29de5ca55528624
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2024-04-17 20:01:41 +0700

    style: products category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,33 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use App\Models\User;
-use App\Service\MailService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
-    public function __construct(
-        protected MailService $mail
-    ) {
-    }
-
-    public function coba()
-    {
-        echo "Halaman coba";
-    }
-
-    public function testing(MailService $mail)
-    {
-        $teks = "Ini teks dari controller";
-
-        // return view('beranda', ['teks' => $teks]);
-        return view('beranda', compact('teks'));
-    }
-
-
-    public function beranda(Request $request)
+    public function products(Request $request)
     {
         $products = Product::with(['product_category'])
             ->when($request->category_id != "", function ($q) use ($request) {
@@ -37,10 +15,5 @@ class HomeController extends Controller
             })->get();
 
         return view('customers.product', compact('products'));
-    }
-
-    public function readFile($filename)
-    {
-        return response()->file("design-system/assets/images/{$filename}");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\UsersController;
-use App\Http\Middleware\AksesBuatBukaDompet;
-use App\Http\Middleware\VerificationMiddleware;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,71 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::prefix('/user')->group(function () {
-//     Route::get('/contoh', function () {
-//         //
-//         $angka = 12;
-
-//         echo $angka;
-//     });
-
-//     Route::get('/teks', function () {
-//         //
-//         $angka = 12;
-
-//         echo $angka;
-//     });
-// });
-
-
-// Route::get('/login', [AuthController::class, 'login']);
-// Route::post('/login/process', [AuthController::class, 'process']);
-// Route::put('/login/process', [AuthController::class, 'process']);
-// Route::patch('/login/process', [AuthController::class, 'process']);
-// Route::delete('/login/process', [AuthController::class, 'process']);
-// Route::options('/login/process', [AuthController::class, 'process']);
-
-
-// Route::middleware(['verif', 'verif_token'])->group(function () {
-//     Route::get('/beranda', [HomeController::class, 'beranda']);
-
-//     Route::get('/controller', [HomeController::class, 'coba']);
-// });
-
-// Route::get('/login', [LoginController::class, 'index']);
-// Route::post('/login/process', [LoginController::class, 'login'])->name('login');
-
-// Route::redirect('/controller', '/beranda', 301);
-// Route::permanentRedirect('/controller', '/beranda');
-
-// Route::view('/beranda', 'beranda');
-
-Route::resources([
-    'users' => UsersController::class
-]);
-
-Route::middleware([AksesBuatBukaDompet::class])->group(function () {
-    Route::get('/dompet', function () {
-        return response()->json(['message' => 'Uang 10.000'], 200)
-            ->withHeaders([
-                'Content-Type' => 'application/json',
-                'kunci-kamar' => 'required',
-                'kunci-lemari' => 'Automatic Generate',
-            ]);
-    });
-});
-
-
-Route::middleware('cache.headers:public;max_age=3600')->group(function () {
-    Route::get('/file/{filename}', [HomeController::class, 'readFile']);
-});
-
-Route::get('/beranda', [HomeController::class, 'beranda']);
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -94,3 +23,5 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/products', [HomeController::class, 'products'])->name('customer.products');
