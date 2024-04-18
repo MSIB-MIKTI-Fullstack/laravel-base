6b01026da42a1b27f499a633151f9612e388d36f
LinggaS <ucihalingga12@gmail.com>
2024-04-18 02:51:09 -0400

    tes

---
+++ resources/views/components/filter-category.blade.php
@@ -0,0 +1,15 @@
+<div>
+    <h4 class="font-medium flex-1 self-center mb-3 text-base">Categories</h4>
+    <form action="">
+        @foreach ($categories as $item)
+            <a href="">
+                <div
+                    class="px-4 py-1 lg:px-4 bg-transparent  text-brand text-base  transition hover:bg-brand-500/10 hover:text-brand-500 border border-slate-200 border-dashed font-medium w-full">
+                    <label class="custom-label inline-block dark:text-slate-300">
+                        <span class="ms-1">{{ $item->name }}</span>
+                    </label>
+                </div>
+            </a>
+        @endforeach
+    </form>
+</div>
