2818c6fa03c617fd8f47928c07bfe3ae09193b99
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2024-04-18 19:43:19 +0700

    add: home customer page

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function home()
+    {
+        return view('customers.home');
+    }
+
+    public function products(Request $request)
+    {
+        $products = Product::with(['product_category'])
+            ->when($request->category_id != "", function ($q) use ($request) {
+                $q->where('product_category_id', $request->category_id);
+            })->paginate(10);
+
+        return view('customers.product', compact('products'));
+    }
+}
