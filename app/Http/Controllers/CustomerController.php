892797a110d05875ccc7a084003c599590fb5c3e
SlimBook13 <Noviana11>
2024-04-19 12:34:24 +0700

    home page

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use App\Models\Product;
 
-class HomeController extends Controller
+class CustomerController extends Controller
 {
     public function testing()
     {
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,4 +25,9 @@ Route::middleware([
     })->name('dashboard');
 });
 
-Route::get('/products', [HomeController::class, 'products'])->name('customer.products');
+Route::get('/products', [CustomerController::class, 'products'])->name('customer.products');
+
+Route::group(['as' => 'customer.'], function () {
+    Route::get('/', [CustomerController::class, 'home'])->name('home');
+    Route::get('/products', [CustomerController::class, 'products'])->name('products');
+});
