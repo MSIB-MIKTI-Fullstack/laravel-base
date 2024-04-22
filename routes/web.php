0734c77ed589a990c5941e38ad5bf5bb45592a7b
rrnaldi <renaldi240402@gmail.com>
2024-04-22 11:12:11 +0700

    add home page, customer page, layout navigation, img asset

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/products', [HomeController::class, 'products'])->name('customer.products');
+Route::group(['as' => 'customer.'], function () {
+    Route::get('/', [CustomerController::class, 'home'])->name('home');
+    Route::get('/products', [CustomerController::class, 'products'])->name('products');
+});
 
 
 Route::middleware([
