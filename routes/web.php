e71bfe36a8fdc9c38747804cd9bf2896e66fbcf3
LinggaS <ucihalingga12@gmail.com>
2024-04-22 08:59:17 -0400

    membuat controller baru

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\Customer\HomeController;
+use App\Http\Controllers\Customer\ProductController;
 use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\SessionControll;
-use App\Http\Controllers\SiswaController;
-use App\Http\Controllers\HomeController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,30 +29,10 @@ use Illuminate\Support\Facades\Route;
 //     })->name('dashboard');
 // });
 
-// //127.0.0.1:8000/siswa ==>  <h1>SAYA SISWA</h1>
-// Route::get('/siswa', function () {
-//     return "<h1>SAYA SISWA</h1>";
-// });
 
-// //127.0.0.1:8000/siswa/id ==>  <h1>SAYA SISWA dengan id</h1>
-// Route::get('/siswa/{id}', function ($id) {
-//     return "<h1>SAYA SISWA dengan ID $id</h1>";
-// })-> where('id', '[0-9]+');
-
-// //127.0.0.1:8000/siswa/id/nama ==>  <h1>SAYA SISWA dengan id dan nama</h1>
-// Route::get('/siswa/{id}/{nama}', function ($id, $nama) {
-//     return "<h1>SAYA SISWA dengan ID $id dan Nama $nama</h1>";
-// })-> where(['id' => '[0-9]+', 'nama' => '[A-Za-z]+']);
-
-Route::get('siswa',[SiswaController::class,'index']);
-
-Route::get('siswa/{id}',[SiswaController::class,'detail']);
-
-Route::get('/sesi',[SessionControll::class,'index']);
-Route::post('/sesi/login',[SessionControll::class,'login']);
 
 
 Route::group(['as' => 'customer.'], function () {
-Route::get('/products', [CustomerController::class, 'products'])->name('products');
-Route::get('/', [CustomerController::class, 'home'])->name('home');
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/products', [ProductController::class, 'index'])->name('products');
 });
