1369c47a0fd4c6f286ee2f7621930c1b4640656e
Rakhadinar Jaladara <rkhdinar00@gmail.com>
2024-04-22 20:38:05 +0700

    fix: split controller home and product

---
+++ app/Http/Controllers/Customer/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Customer;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(Request $request)
+    {
+        $products = Product::with(['product_category'])
+            ->when($request->category_id != "", function ($q) use ($request) {
+                $q->where('product_category_id', $request->category_id);
+            })
+            ->paginate(10)
+            ->appends($request->query());
+
+
+        return view('customers.product', compact('products'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\Customer\HomeController;
+use App\Http\Controllers\Customer\ProductController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -16,8 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['as' => 'customer.'], function () {
-    Route::get('/', [CustomerController::class, 'home'])->name('home');
-    Route::get('/products', [CustomerController::class, 'products'])->name('products');
+    Route::get('/', [HomeController::class, 'home'])->name('home');
+    Route::get('/products', [ProductController::class, 'index'])->name('products');
 });
 
 Route::middleware([
