536d419a63594b0e639bafba7ceef53e1b6d315f
Aurel Izzety <aurelizzety@gmail.com>
2024-04-22 20:57:23 +0700

    add: product-detail etc

--- routes/web.php
+++ routes/web.php
@@ -37,4 +37,5 @@ Route::middleware([
 Route::group(['as' => 'customer.'], function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
     Route::get('/products', [ProductController::class, 'index'])->name('products');
-    Route::get('/products/{slug}', [ProductController::class, 'detail'])->name('product-detail');});
+    Route::get('/products/{slug}', [ProductController::class, 'detail'])->name('product-detail');
+});
