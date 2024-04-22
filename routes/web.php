22a50effb34b3027e18b543b730989e52eecddd9
Velavor <naufalnarukami548@gmail.com>
2024-04-22 21:22:00 +0700

    add:database

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
-use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\Customer\HomeController;
+use App\Http\Controllers\Customer\ProductController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::middleware([
     'auth:sanctum',
@@ -25,6 +16,8 @@ Route::middleware([
 });
 
 Route::group(['as' => 'customer.'], function () {
-    Route::get('/', [CustomerController::class, 'home'])->name('home');
-    Route::get('/products', [CustomerController::class, 'products'])->name('products');
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+
+    Route::get('/products', [ProductController::class, 'index'])->name('products');
+    Route::get('/products/{slug}', [ProductController::class, 'detail'])->name('product-detail');
 });
