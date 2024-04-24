1d1a6ffeef82f39352cd11fce6068aefd0ba974f
Aurel Izzety <aurelizzety@gmail.com>
2024-04-24 21:02:07 +0700

    add: cart fix query

--- app/View/Components/Customers/Cart.php
+++ app/View/Components/Customers/Cart.php
@@ -5,7 +5,7 @@ namespace App\View\Components\Customers;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
-use Illuminate\Support\Facades\DB;
+use App\Models\Cart as ModelsCart;
 use Illuminate\Support\Facades\Auth;
 
 class Cart extends Component
@@ -23,8 +23,9 @@ class Cart extends Component
      */
     public function render(): View|Closure|string
     {
-        $cart = DB::selectOne("SELECT COUNT(c.product_id) as count FROM (SELECT product_id FROM `carts` WHERE user_id = :user_id GROUP BY product_id) as c;", ['user_id' => Auth::user()->id]);
-        $count = $cart->count;
+        $count = ModelsCart::where('user_id', Auth::user()->id)
+            ->distinct('product_id')
+            ->count();
 
         return view('components.customers.cart', compact('count'));
     }
