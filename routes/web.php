900c4996b256b03c67f7d942ca568e1bc608b24f
cahyotriatmojo <111202113617@mhs.dinus.ac.id>
2024-04-24 23:47:09 +0700

    fix: add cart

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,7 @@ Route::group(['as' => 'customer.'], function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::prefix('/products')->group(function() {
         Route::get('/', [ProductController::class, 'index'])->name('products');
-        Route::post('/add-to-cart', [ProductController::class, 'addToCart'])->name('products-add-to-cart');
+        Route::post('/add-to-cart', [ProductController::class, 'addToCart'])->name('product-add-to-cart');
         Route::get('/{slug}', [ProductController::class, 'detail'])->name('product-detail'); //name harus sesuai dgn nama di view digunakan untuk memanggil route di view blade
     });
 
