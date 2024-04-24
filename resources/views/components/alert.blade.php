a6330b2b82bb7d488e6d3072c86fbf09982f65d9
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2024-04-24 20:21:46 +0700

    add: alert component

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,3 +1,11 @@
-<div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
-</div>
+@if (\Session::has('success'))
+    <div class="p-3 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800"
+        role="alert">
+        {!! \Session::get('success') !!}
+    </div>
+@endif
+@if (\Session::has('error'))
+    <div class="p-3 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+        {!! \Session::get('error') !!}
+    </div>
+@endif
