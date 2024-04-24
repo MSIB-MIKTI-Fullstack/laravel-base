addfb7682cbb305a4366c943d4a63b4b4e1e7da0
Sahl <officialisibi@gmail.com>
2024-04-24 20:54:00 +0700

    view add cart

---
+++ app/View/Components/Customers/cart.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\View\Components\customers;
+
+use App\Models\Cart as ModelsCart;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\Component;
+
+class cart extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        $cart_count = DB::table(DB::raw("(SELECT product_id FROM carts WHERE user_id = :user_id GROUP BY product_id) as c"))
+            ->selectRaw("COUNT(c.product_id) as count")
+            ->setBindings(['user_id' => Auth::user()->id], 'where')
+            ->first()->count;
+        // dd($cart_count);
+        return view('components.customers.cart', compact('cart_count'));
+    }
+}
