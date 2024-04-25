0a5fb6bd3fbd96ad355e212e8073b909f925582e
rrnaldi <renaldi240402@gmail.com>
2024-04-25 20:53:21 +0700

    add: cart page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use App\Http\Controllers\Customer\CartController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\Customer\HomeController;
 use App\Http\Controllers\Customer\ProductController;
@@ -12,7 +14,13 @@ Route::group(['as' => 'customer.'], function () {
     Route::prefix('/products')->group(function () {
         Route::get('/', [ProductController::class, 'index'])->name('products');
         Route::get('/{slug}', [ProductController::class, 'detail'])->name('product-detail');
-        Route::post('/add-to-cart', [ProductController::class, 'addToCart'])->name('product-add-to-cart');
+
+        Route::middleware('auth')->group(function () {
+            Route::post('/add-to-cart', [ProductController::class, 'addToCart'])->name('product-add-to-cart');
+        });
+    });
+    Route::middleware('auth')->group(function () {
+        Route::get('/cart', [CartController::class, 'index'])->name('cart');
     });
 });
 
