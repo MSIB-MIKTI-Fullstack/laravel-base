7f0f830fbfd3d60b2f040b774526edfd3d0d474b
DimasPratama03 <112364293+DimasPratama03@users.noreply.github.com>
2024-04-25 14:36:28 +0700

    add : cart route

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,10 @@ Route::middleware([
 
 Route::group(['as' => 'customer.'], function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
-    Route::get('/products', [ProductController::class, 'index'])->name('products');
-    Route::get('/products/{slug}', [ProductController::class, 'detail'])->name('product-detail');
+    
+    Route::prefix('/products')->group(function () {
+        Route::get('/', [ProductController::class, 'index'])->name('products');
+        Route::get('/{slug}', [ProductController::class, 'detail'])->name('product-detail');
+        Route::post('/add-to-cart', [ProductController::class, 'detail'])->name('product-add-to-cart');
+    });
 });
