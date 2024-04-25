941cada38eed189849125558bd05e3a073f9ba98
zelviannisaputri <zelviannisa@gmail.com>
2024-04-26 00:46:45 +0700

    add : route product detail

--- app/Http/Controllers/Customer/ProductController.php
+++ app/Http/Controllers/Customer/ProductController.php
@@ -17,4 +17,10 @@ class ProductController extends Controller
             ->appends($request->query());
         return view('customers.product', compact('products'));
     }
+    public function detail($slug)
+    {
+        $product = Product::where('slug', $slug)->first();
+
+        dd($product);
+    }
 }
