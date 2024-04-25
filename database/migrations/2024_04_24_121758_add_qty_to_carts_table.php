521c599bfddcc37bccd7a4e016e84ea805bf0b69
rrnaldi <renaldi240402@gmail.com>
2024-04-25 07:44:38 +0700

    add: cart count

--- database/migrations/2024_04_24_121758_add_qty_to_carts_table.php
+++ database/migrations/2024_04_24_121758_add_qty_to_carts_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('carts', function (Blueprint $table) {
+        Schema::table('charts', function (Blueprint $table) {
             //
         });
     }
