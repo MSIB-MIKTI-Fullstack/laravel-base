56f9548ee8ba0245f75ae2034a8b518b7a296d6c
aloysiusrio <92454159+aloysiusrio@users.noreply.github.com>
2024-04-25 15:13:16 +0700

    update: product to cart

--- app/Http/Controllers/Customer/HomeController.php
+++ app/Http/Controllers/Customer/HomeController.php
@@ -14,4 +14,11 @@ class HomeController extends Controller
 
         return view('customers.home', compact('products'));
     }
+
+    public function detail($slug)
+    {
+        $product = Product::where('slug', $slug)->first();
+
+        return view('customers.product-detail', compact('product'));
+    }
 }
--- resources/views/customers/home.blade.php
+++ resources/views/customers/home.blade.php
@@ -205,44 +205,45 @@
                 </div><!--end col-->
             </div> <!--end grid-->
             <div class="grid md:grid-cols-12 lg:grid-cols-12 xl:grid-cols-12 gap-4 mb-4">
-                @foreach ($products as $product)
-                    <div class="sm:col-span-12  md:col-span-4 lg:col-span-3 xl:col-span-3 ">
-                        <div
-                            class="bg-white dark:bg-gray-900 border border-slate-200 dark:border-slate-700/40  rounded-md w-full relative">
-                            <div class="flex-auto  text-center">
-                                <div class="flex-auto text-center bg-gray-100">
-                                    <span
-                                        class="absolute right-2 top-2 focus:outline-none text-[12px] bg-green-600/10 text-green-700 dark:text-green-600 rounded font-medium py-0 px-2 mb-5 inline-block">50%
-                                        off</span>
-                                    <a href="#">
-                                        <img src="{{ $product->image }}" alt=""
-                                            class="h-44 inline-block my-4 transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110 duration-500">
-                                    </a>
-                                </div>
-                                <div class="flex-auto  text-center p-4">
-                                    <span
-                                        class="focus:outline-none text-[12px] text-slate-500 border border-slate-200 rounded font-medium py-0 px-2 mb-5 inline-block">{{ $product->product_category->name }}</span>
-                                    <a href="ecommerce-product-detail.html"
-                                        class="text-xl font-semibold text-slate-500 dark:text-gray-400 leading-3 block mb-2">{{ $product->name }}</a>
-                                    <div class="mb-4">
-                                        <i class="icofont-star text-yellow-400 inline-block"></i>
-                                        <i class="icofont-star text-yellow-400 inline-block"></i>
-                                        <i class="icofont-star text-yellow-400 inline-block"></i>
-                                        <i class="icofont-star text-yellow-400 inline-block"></i>
-                                        <i class="icofont-star text-yellow-400 inline-block"></i>
-                                        <span class="text-slate-800 font-semibold">4.8</span>
-                                    </div>
-                                    <h4 class="text-3xl font-medium dark:text-slate-300 mb-4"><sup
-                                            class="text-sm text-slate-500">Rp.
-                                        </sup>{{ number_format($product->price, 0) }}</h4>
-                                    <button type="button"
-                                        class="px-4 py-1 lg:px-4 bg-transparent  text-brand text-base  transition hover:bg-brand-500/10 hover:text-brand-500 border border-slate-200 border-dashed font-medium w-full">Buy
-                                        Now</button>
-                                </div>
-                            </div>
-                        </div> <!--end card-->
-                    </div><!--end col-->
-                @endforeach
+                @foreach ($products as $item)
+                                        <div class="sm:col-span-12  md:col-span-4 lg:col-span-3 xl:col-span-3 ">
+                                            <div class="bg-white border border-slate-200  rounded-md w-full relative">
+                                                <div class="flex-auto  text-center">
+                                                    <div class="flex-auto text-center bg-gray-100">
+                                                        <span
+                                                            class="absolute right-2 top-2 focus:outline-none text-[12px] bg-green-600/10 text-green-700 rounded font-medium py-0 px-2 mb-5 inline-block">50%
+                                                            off</span>
+                                                        <a href="{{route('customer.product-detail', ['slug'=>$item->slug])}}">
+                                                            <img src="{{ $item->image }}" alt=""
+                                                                class="h-44 inline-block my-4 transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110 duration-500">
+                                                        </a>
+                                                    </div>
+                                                    <div class="flex-auto  text-center p-4">
+                                                        <span
+                                                            class="focus:outline-none text-[12px] text-slate-500 border border-slate-200 rounded font-medium py-0 px-2 mb-5 inline-block">{{ $item->product_category->name }}</span>
+                                                        <a href="{{route('customer.product-detail', ['slug'=>$item->slug])}}"
+                                                            class="text-xl font-semibold text-slate-500 leading-3 block mb-2 truncate">{{ $item->name }}</a>
+                                                        <div class="mb-4">
+                                                            <i class="icofont-star text-yellow-400 inline-block"></i>
+                                                            <i class="icofont-star text-yellow-400 inline-block"></i>
+                                                            <i class="icofont-star text-yellow-400 inline-block"></i>
+                                                            <i class="icofont-star text-yellow-400 inline-block"></i>
+                                                            <i class="icofont-star text-yellow-400 inline-block"></i>
+                                                            <span class="text-slate-800 font-semibold">4.8</span>
+                                                        </div>
+                                                        <h4 class="text-3xl font-medium mb-4"><sup
+                                                                class="text-sm text-slate-500">Rp.
+                                                            </sup>{{ number_format($item->price, 0) }}
+                                                        </h4>
+                                                        <button type="button"
+                                                            class="px-4 py-1 lg:px-4 bg-transparent  text-brand text-base  transition hover:bg-brand-500/10 hover:text-brand-500 border border-slate-200 border-dashed font-medium w-full"
+                                                            onclick="location.href='{{route('customer.product-detail', ['slug'=>$item->slug])}}'">Buy
+                                                            Now</button>
+                                                    </div>
+                                                </div>
+                                            </div> <!--end card-->
+                                        </div>
+                                    @endforeach
             </div><!--end inner-grid-->
         </div><!--end container-->
     </div><!--end section-->
