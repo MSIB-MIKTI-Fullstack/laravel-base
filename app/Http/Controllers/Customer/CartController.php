c03a579ba9c4ca6610f857353a4e2a162551fbc9
Sahl <officialisibi@gmail.com>
2024-04-26 10:25:09 +0700

    fix CartController

--- app/Http/Controllers/Customer/CartController.php
+++ app/Http/Controllers/Customer/CartController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class CartController extends Controller
 {
     public function index()
     {
+
+        $cart = Cart::where('user_id', Auth::user()->id)->distinct('product_id')->toArray();
         return view('customers.cart');
     }
 }
