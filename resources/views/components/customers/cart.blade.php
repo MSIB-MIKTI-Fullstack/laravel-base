dd12cc175541b4723987cec5f42abdc5abb6d279
zelviannisaputri <zelviannisa@gmail.com>
2024-04-27 11:13:10 +0700

    chore : button cart component redirect

--- resources/views/components/customers/cart.blade.php
+++ resources/views/components/customers/cart.blade.php
@@ -1,4 +1,4 @@
-<button id="btn-cart" type="button"
+<button onclick="window.location.href = `{{ route('customer.cart') }}`" type="button"
     class="dropdown-toggle flex rounded-full md:me-0 h-10 w-10 items-center justify-center  border-[.5px] dark:border-slate-700/40 bg-[#f4f7ff] text-dark"
     id="Notifications" aria-expanded="false" data-fc-autoclose="both" data-fc-type="dropdown">
     <span data-lucide="shopping-cart" class=" w-5 h-5"></span>
