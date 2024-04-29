9ec153d7e03c538d8cf027d4b95a207c8bfafea0
Aurel Izzety <aurelizzety@gmail.com>
2024-04-29 20:50:42 +0700

    add: update product add to cart

--- app/Http/Controllers/Customer/CartController.php
+++ app/Http/Controllers/Customer/CartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
 use App\Models\Cart;
+use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
@@ -13,5 +14,36 @@ class CartController extends Controller
             ->get();
 
         return view('customers.cart', compact('carts'));
+    }
+
+    public function changeCart(Request $request)
+    {
+        $cart = Cart::find($request->id);
+        try {
+            //code...
+            $cart->update([
+                'qty' => $request->qty
+            ]);
+
+
+
+            return response()->json(['message' => 'Success change cart'], 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json(['message' => $th->getMessage()], 500);
+        }
+    }
+
+    public function getTotalCart()
+    {
+        $carts = Cart::getCartByUser()->get();
+
+        $total = 0;
+
+        foreach ($carts as $key => $item) {
+            $total += $item->total_qty * $item->price;
+        }
+
+        return response()->json(['total' => $total], 200);
     }
 }
