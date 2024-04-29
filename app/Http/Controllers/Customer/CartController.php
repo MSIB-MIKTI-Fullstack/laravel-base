4ba75c04e237bfb8ec99344322a6798c398e16be
Aurel Izzety <aurelizzety@gmail.com>
2024-04-29 19:51:45 +0700

    add: cart controller

--- app/Http/Controllers/Customer/CartController.php
+++ app/Http/Controllers/Customer/CartController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Cart;
 
 class CartController extends Controller
 {
     public function index()
     {
+        $carts = Cart::getCartByUser()
+            ->get();
+
         return view('customers.cart');
     }
 }
--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,9 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Auth;
 
 class Cart extends Model
 {
     use HasFactory;
     protected $guarded = ['created_at', 'updated_at'];
+
+    public function scopeGetCartByUser(Builder $query): void
+    {
+        $query->selectRaw("SUM(qty) as total_qty, name, description, price, image")
+            ->leftJoin('products', 'products.id', '=', 'carts.product_id')
+            ->where('user_id', Auth::user()->id)
+            ->groupBy('product_id');
+    }
 }
