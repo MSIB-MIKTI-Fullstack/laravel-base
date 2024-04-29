e00e0823977805a8cfac545c256a10afb34db8ec
Aurel Izzety <aurelizzety@gmail.com>
2024-04-29 20:23:43 +0700

    add: cart controller

--- app/Http/Controllers/Customer/CartController.php
+++ app/Http/Controllers/Customer/CartController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Cart;
 
 class CartController extends Controller
@@ -13,6 +12,6 @@ class CartController extends Controller
         $carts = Cart::getCartByUser()
             ->get();
 
-        return view('customers.cart');
+        return view('customers.cart', compact('carts'));
     }
 }
