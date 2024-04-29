a597a1cc2b6f3c9c56cd7ee8b74004379c26f1af
rafasepti <rafasepti@gmail.com>
2024-04-29 19:32:39 +0700

    add: controller cart

--- app/Http/Controllers/Customer/CartController.php
+++ app/Http/Controllers/Customer/CartController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cart;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
     public function index()
     {
-        return view('customers.cart');
+        // $carts = Cart::selectRaw('SUM(qty) as total_qty, name, description, price, image')
+        // ->leftJoin('products', 'products.id', '=', 'carts.product_id')
+        // ->where('user_id', Auth::user()->id)
+        // ->groupBy('product_id')
+        // ->get();
+
+        $carts = Cart::getCartByUser()
+            ->get();
+
+        return view('customers.cart', compact('carts'));
     }
 }
