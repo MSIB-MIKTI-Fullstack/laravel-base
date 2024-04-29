758076ca8f417b362b2aec480b7781a407a2635a
DewaAyuu <dwyudiah02@gmail.com>
2024-04-29 20:36:02 +0800

    cart controller

--- app/View/Components/Customers/Cart.php
+++ app/View/Components/Customers/Cart.php
@@ -11,17 +11,6 @@ use Illuminate\View\Component;
 
 class Cart extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
         $count = ModelsCart::where('user_id', Auth::user()->id)
