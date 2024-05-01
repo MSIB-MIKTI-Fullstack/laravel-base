8248f243ef3d5a8f997e0e3e50fb607d8831ede9
Aurel Izzety <aurelizzety@gmail.com>
2024-05-01 20:40:37 +0700

    add: update cart remove

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,7 @@ Route::group(['as' => 'customer.'], function () {
             Route::get('/total-cart', [CartController::class, 'getTotalCart'])->name('total-cart');
             Route::post('/change-cart', [CartController::class, 'changeCart'])->name('change-cart');
             Route::get('/get-cart', [CartController::class, 'getCart'])->name('get-cart');
+            Route::delete('/delete-cart', [CartController::class, 'deleteCart'])->name('delete-cart');
         });
     });
 });
