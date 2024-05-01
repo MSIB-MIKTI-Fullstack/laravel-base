6615dac63e4ec1e50d140793a6e0042415f19e60
indaaa123 <mutiaraindahaisyah@gmail.com>
2024-05-01 14:11:14 +0700

    Update Cart

--- app/Http/Controllers/Customer/CartController.php
+++ app/Http/Controllers/Customer/CartController.php
@@ -3,12 +3,45 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
     public function index()
     {
-        return view('customers.cart');
+        $carts = Cart::getCartByUser()
+            ->get();
+
+        return view('customers.cart', compact('carts'));
+    }
+
+    public function changeCart(Request $request)
+    {
+        $cart = Cart::find($request->id);
+
+        try {
+            $cart->update([
+                'qty' => $request->qty
+            ]);
+
+            return response()->json(['message' => 'Success change cart quantity'], 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json(['message' => $th->getMessage()], 500);
+        }
+    }
+
+    public function getTotalCart()
+    {
+        $carts = Cart::getCartByUser()->get();
+
+        $total = 0;
+
+        foreach ($carts as $key => $item) {
+            $total += $item->total_qty * $item->price;
+        }
+
+        return response()->json(['total' => $total], 200);
     }
 }
