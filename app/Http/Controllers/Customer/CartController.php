b99f4dcb8de9c4f475ab090b0cf11815403b7a42
indahsalim <indahsalim4@gmail.com>
2024-05-01 22:23:36 +0700

    customerproduct

---
+++ app/Http/Controllers/Customer/CartController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Customer;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cart;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function index()
+    {
+        $carts = Cart::getCartByUser()
+            ->get();
+
+        return view('customers.cart', compact('carts'));
+    }
+
+    public function changeCart(Request $request)
+    {
+        $cart = Cart::find($request->id);
+
+        try {
+            $cart->update([
+                'qty' => $request->qty
+            ]);
+
+            return response()->json(['message' => 'Success change cart quantity'], 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response()->json(['message' => $th->getMessage()], 500);
+        }
+    }
+
+    public function getTotalCart()
+    {
+        $carts = Cart::getCartByUser()->get();
+
+        $total = 0;
+
+        foreach ($carts as $key => $item) {
+            $total += $item->total_qty * $item->price;
+        }
+
+        return response()->json(['total' => $total], 200);
+    }
+}
