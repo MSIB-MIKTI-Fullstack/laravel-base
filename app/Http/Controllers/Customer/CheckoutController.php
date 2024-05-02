7b9d48f79e0ad7d89d6725da5d23e971fb56ff98
aloysiusrio <92454159+aloysiusrio@users.noreply.github.com>
2024-05-02 21:15:17 +0700

    add: checkout process

--- app/Http/Controllers/Customer/CheckoutController.php
+++ app/Http/Controllers/Customer/CheckoutController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cart;
+use App\Models\DetailTransaction;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
@@ -10,5 +13,44 @@ class CheckoutController extends Controller
     public function index()
     {
         return view('customers.checkout');
+    }
+
+    public function process(Request $request)
+    {
+        $cart = Cart::getTotalCheckoutByUser()->first();
+
+        try {
+            $transaction = Transaction::create([
+                'first_name' => $request->first_name,
+                'last_name' => $request->last_name,
+                'address' => $request->address,
+                'city' => $request->city,
+                'state' => $request->state,
+                'country' => $request->country,
+                'zip_code' => $request->zip_code,
+                'email_address' => $request->email_address,
+                'phone_number' => $request->phone_number,
+                'total_checkout' => $request->total_checkout,
+                'status' => "pending",
+                'total_checkout' => $cart->total_checkout
+            ]);
+
+            $cart = Cart::getCartByUser()->get();
+
+            foreach ($cart as $key => $item) {
+                DetailTransaction::create([
+                    'transaction_id' => $transaction->id,
+                    'product_id' => $item->product_id,
+                    'user_id' => $item->user_id,
+                ]);
+
+                $item->delete();
+            }
+
+            return redirect()->route('customer.products');
+        } catch (\Throwable $th) {
+            dd($th->getMessage());
+            return redirect()->back();
+        }
     }
 }
---
+++ database/migrations/2024_05_02_125519_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->string('zip_code');
+            $table->string('email_address')->nullable();
+            $table->string('phone_number');
+            $table->integer('total_checkout');
+            $table->string('status');
+            $table->timestamps();
+            $table->text('receipt')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
