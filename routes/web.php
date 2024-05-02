d307aa5047562e15c158e0c751c39aa47268f823
Aurel Izzety <aurelizzety@gmail.com>
2024-05-02 19:21:25 +0700

    add: routing checkout

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Customer\HomeController;
 use App\Http\Controllers\Customer\ProductController;
 use App\Http\Controllers\Customer\CartController;
+use App\Http\Controllers\Customer\CheckoutController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserResourceController;
 
@@ -53,6 +54,10 @@ Route::group(['as' => 'customer.'], function () {
             Route::post('/change-cart', [CartController::class, 'changeCart'])->name('change-cart');
             Route::get('/get-cart', [CartController::class, 'getCart'])->name('get-cart');
             Route::delete('/delete-cart', [CartController::class, 'deleteCart'])->name('delete-cart');
+        });
+
+        Route::group(['prefix' => '/checkout', 'as' => 'checkout.'], function () {
+            Route::get('/', [CheckoutController::class, 'index'])->name('index');
         });
     });
 });
