7c053bf8ae42f4ed4eb8baf3750859b976c2e785
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2024-05-02 20:38:21 +0700

    add: checkout process

--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,7 @@ Route::group(['as' => 'customer.'], function () {
 
         Route::group(['prefix' => '/checkout', 'as' => 'checkout.'], function () {
             Route::get('/', [CheckoutController::class, 'index'])->name('index');
+            Route::post('/process', [CheckoutController::class, 'process'])->name('process');
         });
     });
 });
