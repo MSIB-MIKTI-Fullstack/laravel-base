33745dfa956e77a5aacd20fcf09b65928ee68a84
Sahl <officialisibi@gmail.com>
2024-05-02 19:39:41 +0700

    add: route CO

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\Customer\HomeController;
 use App\Http\Controllers\Customer\CartController;
 use App\Http\Controllers\Customer\ProductController;
+
+use App\Http\Controllers\Customer\CheckoutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -41,6 +43,10 @@ Route::group(['as' => 'customer.'], function () {
             Route::post('/change-cart', [CartController::class, 'changeCart'])->name('change-cart');
             Route::get('/get-cart', [CartController::class, 'getCart'])->name('get-cart');
             Route::delete('/delete-cart', [CartController::class, 'deleteCart'])->name('delete-cart');
+        });
+
+        Route::group(['prefix' => '/checkout', 'as' => 'checkout.'], function () {
+            Route::get('/', [CheckoutController::class, 'index'])->name('index');
         });
     });
 });
