0b6da6665aaab8294ebd43c3081f3baee32f6d89
Sahl <officialisibi@gmail.com>
2024-05-02 20:39:14 +0700

    add: checkout process

--- app/Http/Controllers/Customer/CheckoutController.php
+++ app/Http/Controllers/Customer/CheckoutController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
@@ -10,5 +11,30 @@ class CheckoutController extends Controller
     public function index()
     {
         return view('customers.checkout');
+    }
+
+    public function checkout(Request $request)
+    {
+        try {
+
+
+            Transaction::create([
+                'first_name' => $request->first_name,
+                'last_name' => $request->last_name,
+                'address' => $request->address,
+                'city' => $request->city,
+                'state' => $request->state,
+                'country' => $request->country,
+                'zip_code' => $request->zip_code,
+                'email_address' => $request->email_address,
+                'phone_number' => $request->phone_number,
+                'total_checkout' => $request->total_checkout,
+                'status' => "pending",
+            ]);
+
+
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,7 @@ Route::group(['as' => 'customer.'], function () {
 
         Route::group(['prefix' => '/checkout', 'as' => 'checkout.'], function () {
             Route::get('/', [CheckoutController::class, 'index'])->name('index');
+            Route::get('/process', [CheckoutController::class, 'checkout'])->name('checkout');
         });
     });
 });
