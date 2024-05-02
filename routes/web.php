06f2ea547988ae8d3e830333b2f961283ecef896
Velavor <naufalnarukami548@gmail.com>
2024-05-02 19:24:49 +0700

    add:Routing Checkout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\Customer\CartController;
+use App\Http\Controllers\Customer\CheckoutController;
 use App\Http\Controllers\Customer\HomeController;
 use App\Http\Controllers\Customer\ProductController;
 use Illuminate\Support\Facades\Route;
@@ -32,5 +33,9 @@ Route::group(['as' => 'customer.'], function () {
         Route::post('/change-cart', [CartController::class, 'changeCart'])->name('change-cart');
         Route::get('/get-cart', [CartController::class, 'getCart'])->name('get-cart');
         Route::delete('/delete-cart', [CartController::class, 'deleteCart'])->name('delete-cart');
+    });
+
+    Route::group(['prefix' => '/checkout', 'as' => 'checkout.'], function () {
+        Route::get('/', [CheckoutController::class, 'index'])->name('index');
     });
 });
