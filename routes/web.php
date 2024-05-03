5c479d9a80e8a731a5d3197bd3c12a5c9a1fc731
Fadjaria <202110715203@mhs.ubharajaya.ac.id>
2024-05-03 20:59:58 +0700

    fix:route

--- routes/web.php
+++ routes/web.php
@@ -42,10 +42,10 @@ Route::prefix('/products')->group(function () {
     Route::middleware('auth')->group(function () {
         Route::group(['prefix' => '/cart', 'as' => 'cart.'], function () {
             Route::get('/', [CartController::class, 'index'])->name('index');
-            Route::get('/total-cart', [CartController::class, 'getTotalCart'])->name('cart.total-cart');
-            Route::post('/change-art', [CartController::class, 'changeCart'])->name('cart.change-cart');
-            Route::get('/get-cart', [CartController::class, 'getCart'])->name('cart.get-cart');
-            Route::delete('/delete-cart', [CartController::class, 'deleteCart'])->name('cart.delete-cart');
+            Route::get('/total-cart', [CartController::class, 'getTotalCart'])->name('total-cart');
+            Route::post('/change-art', [CartController::class, 'changeCart'])->name('change-cart');
+            Route::get('/get-cart', [CartController::class, 'getCart'])->name('get-cart');
+            Route::delete('/delete-cart', [CartController::class, 'deleteCart'])->name('delete-cart');
         });
 
         Route::group(['prefix' => '/checkout', 'as' => 'checkout.'], function () {
