161cf4ae8fcb21f63de585a8371f332cf63f074c
rrnaldi <renaldi240402@gmail.com>
2024-05-08 20:41:02 +0700

    update:weight

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -15,7 +15,7 @@ class Cart extends Model
 
     public function scopeGetCartByUser(Builder $query): void
     {
-        $query->selectRaw("carts.id as id, qty as total_qty, name, description, price, image, product_id, user_id")
+        $query->selectRaw("carts.id as id, qty as total_qty, name, description, price, image, product_id, user_id, weight")
             ->leftJoin('products', 'products.id', '=', 'carts.product_id')
             ->where('user_id', Auth::user()->id)
             ->groupBy('product_id');
