afa324e7a8fb83dd02dfaf4ebc74ce34ee56cf15
rrnaldi <renaldi240402@gmail.com>
2024-05-08 19:35:08 +0700

    update: option select checkout

--- app/Http/Controllers/Customer/CheckoutController.php
+++ app/Http/Controllers/Customer/CheckoutController.php
@@ -109,7 +109,7 @@ class CheckoutController extends Controller
 
     public function getCost(Request $request)
     {
-        $origin = "255"; // lokasi kita sekarang
+        $origin = "55"; // lokasi kita sekarang
         $destination = $request->destination;
         $weight = $request->weight;
         $courier = $request->courier;
