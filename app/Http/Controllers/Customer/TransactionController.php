00f960e32e092df2e937c7fe488e9cec5d6db357
rafasepti <rafasepti@gmail.com>
2024-05-09 20:09:20 +0700

    update: pindah file transaction

--- app/Http/Controllers/Customer/TransactionController.php
+++ app/Http/Controllers/Customer/TransactionController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Customer;
 
+use App\Http\Controllers\Controller;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
