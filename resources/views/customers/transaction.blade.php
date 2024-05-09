9492531a6a4afa8899cf8532fd59550697c90068
cahyotriatmojo <111202113617@mhs.dinus.ac.id>
2024-05-09 20:31:34 +0700

    view pending sukses

--- resources/views/customers/transaction.blade.php
+++ resources/views/customers/transaction.blade.php
@@ -16,14 +16,14 @@
                                                 <th>Shipping Cost</th>
                                             </tr>
                                         </thead>
-                                        <tfoot>
+                                        {{-- <tfoot>
                                             <tr>
                                                 <th>ID Transaction</th>
                                                 <th>Status</th>
                                                 <th>Total Checkout</th>
                                                 <th>Shipping Cost</th>
                                             </tr>
-                                        </tfoot>
+                                        </tfoot> --}}
                                     </table>
                                 </div><!--end div-->
                             </div><!--end div-->
@@ -53,10 +53,28 @@
                 {
                     data: 'status',
                     name: 'status',
+                    render: function(data, type, row, meta) {
+                        if (data == "pending") {
+                            return `<span class="bg-gray-500 text-white text-[11px] font-medium mr-1 px-2.5 py-0.5 rounded-full ">Pending</span>`
+                        }
+                        if (data == "process") {
+                            return `<span class="bg-yellow-500 text-white text-[11px] font-medium mr-1 px-2.5 py-0.5 rounded-full ">Process</span>`
+                        }
+                        if (data == "reject") {
+                            return `<span class="bg-red-500 text-white text-[11px] font-medium mr-1 px-2.5 py-0.5 rounded-full ">Reject</span>`
+                        }
+                        if (data == "complete") {
+                            return `<span class="bg-green-500 text-white text-[11px] font-medium mr-1 px-2.5 py-0.5 rounded-full ">Complete</span>`
+                        }
+                        return data;
+                    }
                 },
                 {
                     data: 'total_checkout',
                     name: 'total_checkout',
+                    render: function(data) {
+                        return number_format(data)
+                    }
                 },
                 {
                     data: 'shiping_cost',
