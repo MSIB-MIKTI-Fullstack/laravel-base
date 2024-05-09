680b13291be8720ab10d495ab3275fb44bf4e974
Raja Hariadi <rajahariadi0@gmail.com>
2024-05-09 20:04:12 +0700

    add: page transaction datatable

--- app/Http/Controllers/Customer/TransactionController.php
+++ app/Http/Controllers/Customer/TransactionController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class TransactionController extends Controller
 {
     public function index()
     {
         return view('customers.transaction');
+    }
+
+    public function dataTable(Request $request)
+    {
+        $data = Transaction::all();
+
+        return DataTables::of($data)->toJson();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -82,6 +82,7 @@ Route::group(['as' => 'customer.'], function () {
 
         Route::group(['prefix' => '/transaction', 'as' => 'transaction.'], function () {
             Route::get('/',[TransactionController::class, 'index'])->name('index');
+            Route::get('/datatable',[TransactionController::class, 'dataTable'])->name('datatable');
         });
     });
 });
