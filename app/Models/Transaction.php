2c93bb1efde3471e954876b01976cfe370550732
rrnaldi <renaldi240402@gmail.com>
2024-05-09 20:57:51 +0700

    update:transaction schema table

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,11 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Transaction extends Model
 {
     use HasFactory;
     protected $guarded = ["created_at", "updated_at"];
+
+    public function scopeGetTransactionByUser(Builder $query)
+    {
+        $query->leftJoin('detail_transactions', 'transactions.id', '=', 'detail_transactions.transaction_id')
+            ->where('transactions.user_id', Auth::user()->id);
+    }
 }
