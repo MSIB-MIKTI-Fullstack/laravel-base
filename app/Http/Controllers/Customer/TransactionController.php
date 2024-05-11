c5aeaf4e18544187d8f8359a160f68fa71ce53e6
Fadjaria <202110715203@mhs.ubharajaya.ac.id>
2024-05-11 16:19:19 +0700

    update: transaction schema table

--- app/Http/Controllers/Customer/TransactionController.php
+++ app/Http/Controllers/Customer/TransactionController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class TransactionController extends Controller
 {
     public function index()
     {
         return view('customers.transaction');
+    }
+
+    public function datatable(Request $request)
+    {
+        $data = Transaction::getTransactionByUser()->get();
+        return DataTables::of($data)->toJson();
     }
 }
