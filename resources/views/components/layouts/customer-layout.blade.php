cff57c26fd15653cd5438485c86245e69c959aaf
Fadjaria <202110715203@mhs.ubharajaya.ac.id>
2024-05-11 16:22:02 +0700

    style: navbar customer layouts

--- resources/views/components/layouts/customer-layout.blade.php
+++ resources/views/components/layouts/customer-layout.blade.php
@@ -242,22 +242,10 @@
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="customers-wishlist.html"
-                                            class="flex justify-between py-2 text-base font-medium text-dark hover:text-brand lg:mx-5 lg:inline-flex lg:py-6 2xl:mx-6">
-                                            Wishlist
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="customers-stores.html"
-                                            class="flex justify-between py-2 text-base font-medium text-dark hover:text-brand lg:mx-5 lg:inline-flex lg:py-6 2xl:mx-6">
-                                            Stores
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="customers-checkout.html"
-                                            class="flex justify-between py-2 text-base font-medium text-dark hover:text-brand lg:mx-5 lg:inline-flex lg:py-6 2xl:mx-6">
-                                            Checkout
-                                        </a>
+                                    <a href="{{ route('customer.transaction.index') }}"
+                                            class="flex justify-between py-2 text-base font-medium {{ Route::is('customer.transaction.index') ? 'text-brand' : 'text-dark' }} hover:text-brand lg:mx-5 lg:inline-flex lg:py-6 2xl:mx-6">
+                                            Transaction
+                                    </a>
                                     </li>
                                 </ul>
                             </nav>
