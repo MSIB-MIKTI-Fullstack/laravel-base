e2073d42a7ca2ff681bf752863f97c5796a24fa2
zelviannisaputri <zelviannisa@gmail.com>
2024-05-11 14:00:45 +0700

    add : transaction datatable

--- app/Http/Controllers/Customer/TransactionController.php
+++ app/Http/Controllers/Customer/TransactionController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class TransactionController extends Controller
 {
     public function index()
     {
         return view('customers.transaction');
+    }
+    public function datatable(Request $request)
+    {
+        $data = Transaction::all();
+
+        return DataTables::of($data)->toJson();
     }
 }
