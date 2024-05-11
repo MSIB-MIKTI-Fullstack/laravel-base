bcd6dad06026e705c4a42afe2c48acb758330223
zelviannisaputri <zelviannisa@gmail.com>
2024-05-11 12:41:24 +0700

    update : shipping charge

--- resources/views/customers/checkout.blade.php
+++ resources/views/customers/checkout.blade.php
@@ -52,14 +52,14 @@
                                                     </td>
                                                 </tr>
                                                 <!-- 2 -->
-                                                {{--<tr class="border-b border-dashed border-slate-500/60 dark:border-slate-700/40">
+                                                <tr class="border-b border-dashed border-slate-500/60 dark:border-slate-700/40">
                                                     <td class="p-3 text-sm text-gray-300 whitespace-nowrap font-medium">
                                                         Shipping Charge
                                                     </td>
-                                                    <td class="p-3 text-sm font-medium text-gray-400 whitespace-nowrap">
-                                                        $5.00
+                                                    <td id="shipping-charge" class="p-3 text-sm font-medium text-gray-400 whitespace-nowrap">
+                                                        -
                                                     </td>
-                                                </tr>--}}
+                                                </tr>
                                                 <!-- 3 -->
                                                 {{-- <tr class="">
                                                     <td class="p-3 text-sm text-gray-300 whitespace-nowrap font-medium">
@@ -297,7 +297,10 @@
                                                 </tr>
                 `
                 $('#subtotal').html(number_format(total_price))
-                $('#total').html(number_format(total_price))
+                let shipping_charge = parseInt($('#service').find(':selected').val() == "Select Service" ?
+                    0 : $('#service').find(':selected').val())
+                $('#shipping-charge').html(number_format(shipping_charge))
+                $('#total').html(number_format(total_price + shipping_charge)) 
                 $('#table-cart').html(html)
                 getTotalCart()
             },
@@ -378,6 +381,7 @@
                         `<option value="${item.cost[0].value}">${number_format(item.cost[0].value)} (${item.service}) ${item.description} - Estimate: ${item.cost[0].etd}</option>`
                     )
                 })
+                getCartData()
             },
             error: function(data) {
                 notyf.error(data.message)
@@ -386,5 +390,8 @@
     }
     $('#courier').change(function() {
         getCostOngkir()
+    })
+    $('#service').change(function() {
+        getCartData()
     })
 </script>
