df26517b478b109d59cd377b1fcce3925f17df38
aloysiusrio <92454159+aloysiusrio@users.noreply.github.com>
2024-05-13 20:44:25 +0700

    update: upload receipt

--- app/Http/Controllers/Customer/TransactionController.php
+++ app/Http/Controllers/Customer/TransactionController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Transaction;
 use App\Service\UploadFileService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Yajra\DataTables\Facades\DataTables;
 
 class TransactionController extends Controller
