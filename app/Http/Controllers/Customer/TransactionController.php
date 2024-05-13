67df022964df1e16bf22b26cd1610fbadd00994b
Noviana11 <nopia4747@gmail.com>
2024-05-13 20:51:49 +0700

    update: delete file existing

--- app/Http/Controllers/Customer/TransactionController.php
+++ app/Http/Controllers/Customer/TransactionController.php
@@ -50,6 +50,10 @@ public function uploadReceipt(Request $request)
     $path = $this->uploadFileService->uploadFile($request->file('receipt'));
 
     $transaction = Transaction::find($request->transaction_id);
+
+    if ($transaction->receipt != null) {
+        Storage::delete($transaction->receipt);
+    }
     $transaction->update([
         'status' => 'process',
         'receipt' => $path
