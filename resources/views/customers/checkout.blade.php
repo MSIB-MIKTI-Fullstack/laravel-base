1bdd4aec02bfc9c628a6b96f46c75d6459e69798
rrnaldi <renaldi240402@gmail.com>
2024-05-13 20:19:27 +0700

    add: upload receipt checkout

--- resources/views/customers/checkout.blade.php
+++ resources/views/customers/checkout.blade.php
@@ -184,7 +184,7 @@
                                         <div class="mb-2">
                                             <label for="Email_Address" class="font-medium text-sm text-slate-600 dark:text-slate-400">Email
                                                 Address</label>
-                                            <input class="form-input w-full rounded-md mt-1 border border-slate-300/60 dark:border-slate-700 dark:text-slate-300 bg-transparent px-3 py-2 focus:outline-none focus:ring-0 placeholder:text-slate-400/70 placeholder:font-normal placeholder:text-sm hover:border-slate-400 focus:border-brand-500 dark:focus:border-brand-500  dark:hover:border-slate-700" placeholder="Enter Email" type="text" name="email_addres" required>
+                                            <input class="form-input w-full rounded-md mt-1 border border-slate-300/60 dark:border-slate-700 dark:text-slate-300 bg-transparent px-3 py-2 focus:outline-none focus:ring-0 placeholder:text-slate-400/70 placeholder:font-normal placeholder:text-sm hover:border-slate-400 focus:border-brand-500 dark:focus:border-brand-500  dark:hover:border-slate-700" placeholder="Enter Email" type="text" name="email_address" required>
                                         </div>
                                     </div>
                                     <div class="col-span-4 md:col-span-2 lg:col-span-1 xl:col-span-1">
--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,7 @@ Route::group(['as' => 'customer.'], function () {
         Route::group(['prefix' => '/transaction', 'as' => 'transaction.'], function () {
             Route::get('/', [TransactionController::class, 'index'])->name('index');
             Route::get('/datatable', [TransactionController::class, 'datatable'])->name('datatable');
+            Route::post('/upload-receipt', [TransactionController::class, 'uploadReceipt'])->name('upload-receipt');
         });
     });
 });
