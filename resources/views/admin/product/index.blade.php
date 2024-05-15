6d43d98098f163b6b0c06782ddbb7e0a25c90b16
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2024-05-15 20:35:23 +0700

    add: icon edit & delete

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -25,12 +25,49 @@
             </div> <!--end grid-->
         </div><!--end container-->
     </div>
+
+    <div class="modal animate-ModalSlide hidden" id="modalcenter">
+        <div
+            class="relative w-auto pointer-events-none sm:max-w-lg sm:my-0 sm:mx-auto z-[99] flex items-center h-[calc(100%-3.5rem)]">
+            <form action="{{ route('admin.product.destroy', 'id') }}" method="POST" enctype="multipart/form-data">
+                @method('DELETE')
+                @csrf
+                <div
+                    class="relative flex flex-col w-full pointer-events-auto bg-white dark:bg-slate-800 bg-clip-padding rounded">
+                    <div
+                        class="flex shrink-0 items-center justify-between py-2 px-4 rounded-t border-b border-solid dark:border-gray-700 bg-slate-800">
+                        <h6 class="mb-0 leading-4 text-base font-semibold text-slate-300 mt-0" id="staticBackdropLabel1">
+                            Delete Product</h6>
+                        <button type="button"
+                            class="box-content w-4 h-4 p-1 bg-slate-700/60 rounded-full text-slate-300 leading-4 text-xl close"
+                            aria-label="Close" data-fc-dismiss>&times;</button>
+                    </div>
+                    <div class="relative flex-auto p-4 text-slate-600 dark:text-gray-300 leading-relaxed">
+                        <input type="hidden" name="product_id" id="product_id">
+                        <p>Are you sure to delete?</p>
+                    </div>
+                    <div
+                        class="flex flex-wrap shrink-0 justify-end p-3  rounded-b border-t border-dashed dark:border-gray-700">
+                        <button type="button"
+                            class="inline-block focus:outline-none text-red-500 hover:bg-red-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-red-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-red-500  text-sm font-medium py-1 px-3 rounded mr-1 close"
+                            data-fc-dismiss>Close</button>
+                        <button type="submit"
+                            class="inline-block focus:outline-none text-primary-500 hover:bg-primary-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-primary-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-primary-500  text-sm font-medium py-1 px-3 rounded">Yes</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
 @section('script')
     <script>
         $(document).ready(function() {
             initializeTable()
         })
+
+        function openModal(id) {
+            $('#product_id').val(id)
+        }
 
         function initializeTable() {
             table = new DataTable('#data-table', {
@@ -59,7 +96,8 @@
                     }
                 ],
                 drawCallback: function(settings) {
-                    //
+                    reinitializeScript()
+                    lucide.createIcons();
                 }
             });
         }
