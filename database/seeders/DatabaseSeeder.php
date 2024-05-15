bea6e5146f1cc82507ac75f707617b86a18b6c73
rafasepti <rafasepti@gmail.com>
2024-05-15 19:25:40 +0700

    add: role to users migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
+        //\App\Models\User::factory(10)->create();
         
 
         // \App\Models\User::factory()->create([
@@ -28,9 +28,21 @@ class DatabaseSeeder extends Seeder
             CartSeeder::class
         ]);
 
-        \App\Models\User::factory(1)->create([
+        \App\Models\User::create([
             'name' => 'Rafa Septiani',
             'email' => 'rafasepti@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('qwerty1234'), // password
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null,
+        ]);
+
+        \App\Models\User::create([
+            'name' => 'Admininstrator',
+            'email' => 'admin@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('qwerty1234'), // password
             'two_factor_secret' => null,
