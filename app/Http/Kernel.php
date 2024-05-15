27bd656a9d46c7d7797c5d77a1bcdc7dba2edd98
Noviana11 <nopia4747@gmail.com>
2024-05-15 19:50:06 +0700

    add: route admin and middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'verif' => VerificationMiddleware::class,
+        'verif_token' => VerikasiTokenMiddleware::class,
+        'admin' => \App\Http\Middleware\EnsureIsAdmin::class,
     ];
 }
