2ae5d234df6be2fb873845a58b3306b673b20a34
Noviana11 <nopia4747@gmail.com>
2024-05-15 20:42:48 +0700

    update admin

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,16 +3,35 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('admin.product.index');
+        if ($request->ajax()) {
+            $data = Product::with('product_category')->get();
+
+            return DataTables::of($data)
+                ->addColumn('action', function ($row) {
+                    return '<div class="flex flex-row items-center gap-2">
+                    <a href="' . route('admin.product.edit', $row->id) . '">
+                        <i data-lucide="pen" class="top-icon w-5 h-5 text-green-500"></i>
+                    </a>
+                    <button type="button" data-fc-type="modal" data-fc-target="modalcenter"
+                        onclick="openModal(' . $row->id . ')">
+                        <i data-lucide="trash" class="top-icon w-5 h-5 text-red-500"></i>
+                    </button>
+                </div>';
+                })
+                ->rawColumns(['action'])
+                ->toJson();
+        }
     }
 
     /**
@@ -58,8 +77,8 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $request, string $id)
     {
-        //
+        dd($request->all());
     }
 }
