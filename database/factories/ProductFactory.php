288e4fd7478421479dc372cda950bfd761352ea6
DewaAyuu <dwyudiah02@gmail.com>
2024-05-15 19:34:44 +0800

    update: product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -27,6 +27,8 @@ class ProductFactory extends Factory
             'image' => fake()->imageUrl(640, 640),
             'price' => fake()->numberBetween(10000, 100000),
             'slug' => Str::slug($name),
-            'product_category_id' => $product_category->id        ];
+            'product_category_id' => $product_category->id,
+            'weight' => fake()->numberBetween(100, 1000),
+        ];
     }
 }
