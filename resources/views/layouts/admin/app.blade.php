7f4bcaa18a46d50013a1ffee3ea4c4eaa4c69212
DewaAyuu <dwyudiah02@gmail.com>
2024-05-15 22:26:58 +0800

    add: number format admin layout

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -1101,6 +1101,13 @@
                 appendScript()
             }
         }
+
+        function number_format(number) {
+            return Intl.NumberFormat("id-ID", {
+                style: "currency",
+                currency: "IDR"
+            }).format(number)
+        }
     </script>
     @yield('script')
     <!-- JAVASCRIPTS -->
