966d6f30d2f3239220427651aa4d5e60031dcee0
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2024-05-15 20:04:53 +0700

    add: product admin page

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\ProductController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'Halo admin';
+Route::group(['as' => 'admin.'], function () {
+    Route::resources([
+        'product' => ProductController::class,
+    ]);
 });
