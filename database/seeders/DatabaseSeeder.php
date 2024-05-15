6e9e0ded94f972e634814552096682f5264fc61d
MohammadIndraA <akunpertamasaya5@gmail.com>
2024-05-15 19:26:26 +0700

    add: column role to table user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Str;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -13,16 +14,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::create([
+            'name' => 'Bambang Oke',
+            'email' => 'bambang@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'role' =>'admin',
+            'two_factor_secret' => null,
+            'two_factor_recovery_codes' => null,
+            'remember_token' => Str::random(10),
+            'profile_photo_path' => null,
+            'current_team_id' => null,
+        ]);
         $this->call([
-            ProductCategorySeeder::class,
-            CartSeeder::class,
-            ProductSeeder::class,
+            // ProductCategorySeeder::class,
+            // CartSeeder::class,
+            // ProductSeeder::class,
         ]);
     }
 }
