c4d376e209093a4221430b48b87f194251fcbc50
rrnaldi <renaldi240402@gmail.com>
2024-05-15 20:45:17 +0700

    add: icon and edit delete

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\TryCatch;
 use Yajra\DataTables\Facades\DataTables;
 
 class ProductController extends Controller
@@ -19,7 +20,15 @@ class ProductController extends Controller
 
             return DataTables::of($data)
                 ->addColumn('action', function ($row) {
-                    return '';
+                    return '<div class="flex flex-row items-center gap-2">
+                    <a href="' . route('admin.product.edit', $row->id) . '">
+                        <i data-lucide="pen" class="top-icon w-5 h-5 text-green-500"></i>
+                    </a>
+                    <button type="button" data-fc-type="modal" data-fc-target="modalcenter"
+                        onclick="openModal(' . $row->id . ')">
+                        <i data-lucide="trash" class="top-icon w-5 h-5 text-red-500"></i>
+                    </button>
+                </div>';
                 })
                 ->rawColumns(['action'])
                 ->toJson();
@@ -70,8 +79,7 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $request, string $id)
     {
-        //
     }
 }
