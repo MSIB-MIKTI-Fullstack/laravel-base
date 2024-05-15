f7db17f0636d264d229b7251c72264ef0fbf6472
rafasepti <rafasepti@gmail.com>
2024-05-15 21:21:51 +0700

    add: number format admin layout

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -1102,6 +1102,13 @@
                 appendScript()
             }
         }
+
+        function number_format(number) {
+            return Intl.NumberFormat("id-ID", {
+                style: "currency",
+                currency: "IDR"
+            }).format(number)
+        }
     </script>
     @yield('script')
     <!-- JAVASCRIPTS -->
