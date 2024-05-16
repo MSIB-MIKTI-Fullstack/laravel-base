ef39e05cb36f1b81333cf58d763d93823343c739
ASNIWATI <asniwati0127@gmail.com>
2024-05-16 23:15:28 +0700

    feat: crud product categories

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -9,6 +9,7 @@ class ProductCategory extends Model
 {
     use HasFactory;
 
+    protected $guarded = ['created_at', 'updated_at'];
     public function product()
     {
         return $this->hasMany(Product::class);
--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\ProductCategoryController;
 use App\Http\Controllers\Admin\ProductController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
 Route::group(['as' => 'admin.'], function () {
     Route::resources([
         'product' => ProductController::class,
+        'product-categories' => ProductCategoryController::class,
     ]);
 });
