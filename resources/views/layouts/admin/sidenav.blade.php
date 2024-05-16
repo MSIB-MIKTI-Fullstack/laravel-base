088d93c7bb078cd2d84f966ac279a80757734c10
Raja Hariadi <rajahariadi0@gmail.com>
2024-05-16 20:50:13 +0700

    feat: users crud admin

--- resources/views/layouts/admin/sidenav.blade.php
+++ resources/views/layouts/admin/sidenav.blade.php
@@ -37,5 +37,27 @@
                 </ul>
             </div>
         </div>
+        <a href="#"
+            class="nav-link hover:bg-transparent hover:text-black  rounded-md   flex items-center  decoration-0 px-3 py-3 cursor-pointer group-data-[sidebar=dark]:hover:text-slate-200 group-data-[sidebar=brand]:hover:text-slate-200"
+            data-fc-type="collapse" data-fc-parent="parent-accordion">
+            <span data-lucide="user"
+                class="w-5 h-5 text-center text-slate-800 me-2 group-data-[sidebar=dark]:text-slate-400 group-data-[sidebar=brand]:text-slate-400"></span>
+            <span>Master Users</span>
+            <i
+                class="icofont-thin-down ms-auto inline-block text-[14px] transform transition-transform duration-300 text-slate-800 group-data-[sidebar=dark]:text-slate-400 group-data-[sidebar=brand]:text-slate-400 fc-collapse-open:rotate-180 "></i>
+        </a>
+
+        <div id="Admin-flush" class=" overflow-hidden">
+            <ul class="nav flex-col flex flex-wrap ps-0 mb-0 ms-2">
+                <li class="nav-item relative block">
+                    <a href="{{ route('admin.users.index') }}"
+                        class="nav-link hover:text-primary-500 rounded-md relative   flex items-center decoration-0 px-3 py-3 group-data-[sidebar=brand]:hover:text-slate-200">
+                        <i
+                            class="icofont-dotted-right me-2 text-slate-600 text-[8px] group-data-[sidebar=brand]:text-slate-400 "></i>
+                        Users
+                    </a>
+                </li>
+            </ul>
+        </div>
     </li>
 </ul>
