297066937240e02639dfe8271367999e7540fc0f
rafasepti <rafasepti@gmail.com>
2024-05-16 19:48:39 +0700

    feat: crud product categories

--- app/Http/Controllers/Admin/ProductCategoryController.php
+++ app/Http/Controllers/Admin/ProductCategoryController.php
@@ -3,16 +3,36 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProductCategory;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class ProductCategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        if ($request->ajax()) {
+            $data = ProductCategory::all();
+
+            return DataTables::of($data)
+                ->addColumn('action', function ($row) {
+                    return '<div class="flex flex-row items-center gap-2">
+                        <a href="' . route('admin.product-categories.edit', $row->id) . '">
+                            <i data-lucide="pen" class="top-icon w-5 h-5 text-green-500"></i>
+                        </a>
+                        <button type="button" data-fc-type="modal" data-fc-target="modalcenter"
+                            onclick="openModal(' . $row->id . ')">
+                            <i data-lucide="trash" class="top-icon w-5 h-5 text-red-500"></i>
+                        </button>
+                    </div>';
+                })
+                ->rawColumns(['action'])
+                ->toJson();
+        }
+        return view('admin.product_category.index');
     }
 
     /**
@@ -20,7 +40,7 @@ class ProductCategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.product_category.add');
     }
 
     /**
@@ -28,7 +48,15 @@ class ProductCategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            ProductCategory::create([
+                'name' => $request->name,
+            ]);
+
+            return redirect()->route('admin.product-categories.index')->with('success', "Product Category Added");
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', $th->getMessage());
+        }
     }
 
     /**
@@ -44,7 +72,9 @@ class ProductCategoryController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $data = ProductCategory::find($id);
+
+        return view('admin.product_category.edit', compact('data'));
     }
 
     /**
@@ -52,14 +82,33 @@ class ProductCategoryController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        try {
+
+            $data = ProductCategory::find($id);
+
+            $data->update([
+                'name' => $request->name,
+            ]);
+
+            return redirect()->route('admin.product-categories.index')->with('success', "Product Category Updated");
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', $th->getMessage());
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $request, string $id)
     {
-        //
+        try {
+            $data = ProductCategory::find($request->product_category_id);
+
+            $data->delete();
+
+            return redirect()->back()->with('success', "Product Category deleted");
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', $th->getMessage());
+        }
     }
 }
--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class ProductCategory extends Model
 {
     use HasFactory;
+    protected $guarded = ['created_at', 'updated_at'];
 
     public function product(){
         return $this->hasMany(Product::class);
