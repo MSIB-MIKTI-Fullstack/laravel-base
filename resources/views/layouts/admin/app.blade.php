8f81a99ca594683107a430049629647e769bec93
MohammadIndraA <akunpertamasaya5@gmail.com>
2024-05-16 20:06:40 +0700

    add: breadcrum to app admin

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -77,19 +77,16 @@
                     <div class="flex items-center py-4 w-full">
                         <div class="w-full">
                             <div class="">
-                                {{-- <div class="flex flex-wrap justify-between">
-                                        <div class="items-center ">
-                                            <h1 class="font-medium text-3xl block">@yield('title_page')</h1>
-                                            <ol class="list-reset flex text-sm">
-                                              @yield('breadcrum')
-                                            </ol>
-                                        </div>
-                                        <div class="flex items-center">
-                                            <div class="today-date leading-5 mt-2 lg:mt-0 form-input w-auto rounded-md border inline-block border-primary-500/60 text-primary-500 bg-transparent px-3 py-1 focus:outline-none focus:ring-0 placeholder:text-slate-400/70 placeholder:font-normal placeholder:text-sm hover:border-primary-400 focus:border-primary-500 ">
-                                                <input type="text" class="dash_date border-0 focus:border-0 focus:outline-none" readonly  required="">
-                                            </div>
-                                        </div>
-                                    </div> --}}
+                                <div class="flex flex-wrap justify-between">
+                                    <div class="items-center ">
+                                        <h1 class="font-medium text-3xl block">@yield('title_page')
+                                        </h1>
+                                        <ol class="list-reset flex text-sm">
+                                            @yield('breadcrumb')
+                                        </ol>
+                                    </div>
+
+                                </div>
                             </div>
                         </div>
                     </div>
