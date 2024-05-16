b31c60cbc6bb25fd3a1d8d4201262ad2fc69e41e
mcsans <mihsansaepulloh9@gmail.com>
2024-05-16 20:56:55 +0700

    Feat: users crud admin

--- routes/admin.php
+++ routes/admin.php
@@ -2,11 +2,13 @@
 
 use App\Http\Controllers\Admin\ProductCategoryController;
 use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['as' => 'admin.'], function () {
     Route::resources([
         'product' => ProductController::class,
         'product-categories' => ProductCategoryController::class,
+        'users' => UserController::class,
     ]);
 });
