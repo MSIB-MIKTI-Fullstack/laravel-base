4e02ba6e4fab5cfdf6dacced8d73cae8be545d1f
rafasepti <rafasepti@gmail.com>
2024-05-16 20:53:32 +0700

    feat: users crud admin

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\ProductCategoryController;
 use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -9,5 +10,6 @@ Route::group(['as' => 'admin.'], function () {
     Route::resources([
         'product' => ProductController::class,
         'product-categories' => ProductCategoryController::class,
+        'users' => UserController::class,
     ]);
 });
