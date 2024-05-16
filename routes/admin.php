c553daff70e869fb312a041d383f4168f8bed898
aloysiusrio <92454159+aloysiusrio@users.noreply.github.com>
2024-05-16 19:51:17 +0700

    add: admin product and admin product category crud

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\ProductCategoryController;
+use App\Http\Controllers\admin\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Halo dunia!';
+Route::group(['as' => 'admin.'], function () {
+    Route::resources([
+        'product' => ProductController::class,
+        'product-categories' => ProductCategoryController::class,
+    ]);
 });
