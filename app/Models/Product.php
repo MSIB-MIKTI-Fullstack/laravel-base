fd84f1d80cf0d6ebd9972b80b19b7c42e0ee564e
LinggaS <ucihalingga12@gmail.com>
2024-05-16 07:52:52 -0400

    add

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,7 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $guarded = ['created_at', 'updated_at'];
     public function product_category()
     {
         return $this->belongsTo(ProductCategory::class);
