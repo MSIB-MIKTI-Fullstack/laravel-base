b654e2dd3136c2fdec724d2a78d5f49caa1b9375
Fadjaria <202110715203@mhs.ubharajaya.ac.id>
2024-05-17 23:04:36 +0700

    feat: add & update product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Dotenv\Repository\Adapter\GuardedWriter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['created_at', 'updated_at'];
 
     public function product_category()
     {
