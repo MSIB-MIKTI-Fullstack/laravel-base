c7b343142b7680283b880ef955996764987d31a8
Velavor <naufalnarukami548@gmail.com>
2024-05-20 20:00:37 +0700

    FIx:admin Transactions

--- routes/admin.php
+++ routes/admin.php
@@ -12,8 +12,9 @@ Route::group(['as' => 'admin.'], function () {
         'product-categories' => ProductCategoryController::class,
         'users' => UserController::class,
     ]);
-});
-Route::group(['prefix' => '/transactions', 'as' => 'transactions.'], function () {
-    Route::get('/', [TransactionController::class, 'index'])->name('index');
-    Route::post('/process-transaction', [TransactionController::class, 'processTransaction'])->name('process-transaction');
+    Route::group(['prefix' => '/transactions', 'as' => 'transactions.'], function () {
+        Route::get('/', [TransactionController::class, 'index'])->name('index');
+        Route::post('/process-transaction', [TransactionController::class, 'processTransaction'])->name('process-transaction');
+    });
+
 });
