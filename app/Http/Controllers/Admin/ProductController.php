681acb833bf23c3a6c9b03d10f55d86b4503a251
DimasPratama03 <112364293+DimasPratama03@users.noreply.github.com>
2024-05-20 18:16:21 +0700

    add: icon edit & delete

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -19,12 +19,20 @@ class ProductController extends Controller
 
             return DataTables::of($data)
                 ->addColumn('action', function ($row) {
-                    return '';
+                    return '<div class="flex flex-row items-center gap-2">
+                        <a href="' . route('admin.product.edit', $row->id) . '">
+                            <i data-lucide="pen" class="top-icon w-5 h-5 text-green-500"></i>
+                        </a>
+                        <button type="button" data-fc-type="modal" data-fc-target="modalcenter"
+                            onclick="openModal(' . $row->id . ')">
+                            <i data-lucide="trash" class="top-icon w-5 h-5 text-red-500"></i>
+                        </button>
+                    </div>';
                 })
                 ->rawColumns(['action'])
                 ->toJson();
         }
-        
+
         return view('admin.product.index');
     }
 
@@ -71,8 +79,8 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $request, string $id)
     {
-        //
+        dd($request->all());
     }
 }
