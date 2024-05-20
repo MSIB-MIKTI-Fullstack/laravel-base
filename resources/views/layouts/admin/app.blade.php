9163d7bafabbff9dcd580758e6bfe3784a3f4835
rrnaldi <renaldi240402@gmail.com>
2024-05-20 19:26:43 +0700

    fix: bug sidenav

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -101,6 +101,12 @@
             } else {
                 appendScript()
             }
+
+            reinitializeSidenav()
+        }
+
+        function reinitializeSidenav() {
+            $('#sidenav-list').html($('#sidenav-list').html())
         }
 
         function number_format(number) {
