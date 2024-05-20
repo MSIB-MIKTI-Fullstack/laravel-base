bad8dcc396879d3abae9f19d32c623937cae9917
Raja Hariadi <rajahariadi0@gmail.com>
2024-05-20 19:59:01 +0700

    add: transaction process

--- resources/views/customers/transaction.blade.php
+++ resources/views/customers/transaction.blade.php
@@ -27,22 +27,33 @@
         </div>
     </div><!--end page-wrapper-->
     <div class="modal animate-ModalSlide hidden" id="modalcenter">
-        <div class="relative w-auto pointer-events-none sm:max-w-lg sm:my-0 sm:mx-auto z-[99] flex items-center h-[calc(100%-3.5rem)]">
-            <form action="{{ route('customer.transaction.upload-receipt') }}" method="POST" enctype="multipart/form-data">
+        <div
+            class="relative w-auto pointer-events-none sm:max-w-lg sm:my-0 sm:mx-auto z-[99] flex items-center h-[calc(100%-3.5rem)]">
+            <form action="{{ route('customer.transaction.upload-receipt') }}" method="POST"
+                enctype="multipart/form-data">
                 @csrf
-                <div class="relative flex flex-col w-full pointer-events-auto bg-white dark:bg-slate-800 bg-clip-padding rounded">
-                    <div class="flex shrink-0 items-center justify-between py-2 px-4 rounded-t border-b border-solid dark:border-gray-700 bg-slate-800">
-                        <h6 class="mb-0 leading-4 text-base font-semibold text-slate-300 mt-0" id="staticBackdropLabel1">
+                <div
+                    class="relative flex flex-col w-full pointer-events-auto bg-white dark:bg-slate-800 bg-clip-padding rounded">
+                    <div
+                        class="flex shrink-0 items-center justify-between py-2 px-4 rounded-t border-b border-solid dark:border-gray-700 bg-slate-800">
+                        <h6 class="mb-0 leading-4 text-base font-semibold text-slate-300 mt-0"
+                            id="staticBackdropLabel1">
                             Upload Receipt</h6>
-                        <button type="button" class="box-content w-4 h-4 p-1 bg-slate-700/60 rounded-full text-slate-300 leading-4 text-xl close" aria-label="Close" data-fc-dismiss>&times;</button>
+                        <button type="button"
+                            class="box-content w-4 h-4 p-1 bg-slate-700/60 rounded-full text-slate-300 leading-4 text-xl close"
+                            aria-label="Close" data-fc-dismiss>&times;</button>
                     </div>
                     <div class="relative flex-auto p-4 text-slate-600 dark:text-gray-300 leading-relaxed">
                         <input type="hidden" name="transaction_id" id="transaction_id">
                         <input type="file" name="receipt" />
                     </div>
-                    <div class="flex flex-wrap shrink-0 justify-end p-3  rounded-b border-t border-dashed dark:border-gray-700">
-                        <button class="inline-block focus:outline-none text-red-500 hover:bg-red-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-red-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-red-500  text-sm font-medium py-1 px-3 rounded mr-1 close" data-fc-dismiss>Close</button>
-                        <button type="submit" class="inline-block focus:outline-none text-primary-500 hover:bg-primary-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-primary-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-primary-500  text-sm font-medium py-1 px-3 rounded">Save</button>
+                    <div
+                        class="flex flex-wrap shrink-0 justify-end p-3  rounded-b border-t border-dashed dark:border-gray-700">
+                        <button
+                            class="inline-block focus:outline-none text-red-500 hover:bg-red-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-red-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-red-500  text-sm font-medium py-1 px-3 rounded mr-1 close"
+                            data-fc-dismiss>Close</button>
+                        <button type="submit"
+                            class="inline-block focus:outline-none text-primary-500 hover:bg-primary-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-primary-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-primary-500  text-sm font-medium py-1 px-3 rounded">Save</button>
                     </div>
                 </div>
             </form>
@@ -78,6 +89,9 @@
                         }
                         if (data == "process") {
                             return `<span class="bg-yellow-500 text-white text-[11px] font-medium mr-1 px-2.5 py-0.5 rounded-full ">Process</span>`
+                        }
+                        if (data == "delivery") {
+                            return `<span class="bg-orange-500 text-white text-[11px] font-medium mr-1 px-2.5 py-0.5 rounded-full ">Complete</span>`
                         }
                         if (data == "reject") {
                             return `<span class="bg-red-500 text-white text-[11px] font-medium mr-1 px-2.5 py-0.5 rounded-full ">Reject</span>`
