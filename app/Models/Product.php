722f4f27f2976e09d16a7b41263ac95f05cfd910
DimasPratama03 <112364293+DimasPratama03@users.noreply.github.com>
2024-05-20 18:25:22 +0700

    feat: add & update product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['created_at', 'updated_at'];
     
     public function product_category()
     {
