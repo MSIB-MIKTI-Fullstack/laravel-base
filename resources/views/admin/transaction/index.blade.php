779ab375957f64e5e8e5ef842513170e50e129c0
Aurel Izzety <aurelizzety@gmail.com>
2024-05-20 20:05:54 +0700

    style: input label receipt number

--- resources/views/admin/transaction/index.blade.php
+++ resources/views/admin/transaction/index.blade.php
@@ -92,7 +92,7 @@
         $('#switch-valid').change(function() {
             if ($(this).is(':checked')) {
                 $('#receipt-number-input').html(`
-                <label for="receipt_number" class="font-medium text-sm text-slate-600 dark:text-slate-400">Receipt Name</label>
+                <label for="receipt_number" class="font-medium text-sm text-slate-600 dark:text-slate-400">Receipt Number</label>
                 <input type="text" id="receipt_number" name="receipt_number"
                     class="form-input w-full rounded-md mt-1 border border-slate-300/60 dark:border-slate-700 dark:text-slate-300 bg-transparent px-3 py-1 focus:outline-none focus:ring-0 placeholder:text-slate-400/70 placeholder:font-normal placeholder:text-sm hover:border-slate-400 focus:border-primary-500 dark:focus:border-primary-500  dark:hover:border-slate-700"
                     required="">
