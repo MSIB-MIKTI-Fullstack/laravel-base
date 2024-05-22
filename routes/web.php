b34d78138bc13e22a85c7cca6ccb633c50f2b79e
Raja Hariadi <rajahariadi0@gmail.com>
2024-05-22 21:10:24 +0700

    add: review transaction

--- routes/web.php
+++ routes/web.php
@@ -86,6 +86,7 @@ Route::group(['as' => 'customer.'], function () {
             Route::post('/upload-receipt', [TransactionController::class, 'uploadReceipt'])->name('upload-receipt');
             Route::post('/complete-transaction', [TransactionController::class, 'completeTransaction'])->name('complete-transaction');
             Route::get('/get-detail-product-transaction', [TransactionController::class, 'getDetailProductTransaction'])->name('get-detail-product-transaction');
+            Route::post('/review-transaction', [TransactionController::class, 'reviewTransaction'])->name('review-transaction');
         });
     });
 });
