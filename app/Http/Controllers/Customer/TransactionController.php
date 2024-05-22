7a7e1c47425ba5bff201ac091efb9999fb0bf1f2
DewaAyuu <dwyudiah02@gmail.com>
2024-05-22 21:17:05 +0800

    update: transaction complete customer

--- app/Http/Controllers/Customer/TransactionController.php
+++ app/Http/Controllers/Customer/TransactionController.php
@@ -28,18 +28,38 @@ class TransactionController extends Controller
             ->addColumn('receipt', function ($row) {
                 $icon = "";
 
-                if ($row->receipt != null) {
-                    $url = Storage::url($row->receipt);
-
-                    $icon = '<a href="' . $url . '" target="_blank">
-                    <i data-lucide="file" class="top-icon w-5 h-5 text-gray-500"></i>
-                    </a>';
+                if ($row->receipt_number != null && $row->status == "delivery") {
+                    return '
+                    <div class="flex flex-col items-center gap-2">
+                        <small> Receipt number: ' . $row->receipt_number . '</small>
+                        <form action="' . route('customer.transaction.complete-transaction') . '" method="POST">
+                            <input type="hidden" name="_token" value="' . csrf_token() . '">
+                            <input type="hidden" name="transaction_id" value="' . $row->id . '" />
+                            <button type="submit"
+                                class="inline-block focus:outline-none text-white-500 hover:bg-green-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-slate-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-slate-500  text-sm font-medium py-1 px-3 rounded">
+                                Complete Transaction
+                            </button>
+                        </form>
+                    </div>
+                    ';
                 }
 
-                return '<div class="flex flex-col items-center gap-2">' . $icon . '<button type="button" data-fc-type="modal" data-fc-target="modalcenter"
-                class="inline-block focus:outline-none text-slate-500 hover:bg-slate-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-slate-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-slate-500  text-sm font-medium py-1 px-3 rounded" onclick="openModal(' . $row->id . ')">
-                Upload Receipt
-            </button>' . '</div>';
+                if ($row->status == "pending" || $row->status == "process") {
+                    if ($row->receipt != null) {
+                        $url = Storage::url($row->receipt);
+
+                        $icon = '<a href="' . $url . '" target="_blank">
+                        <i data-lucide="file" class="top-icon w-5 h-5 text-gray-500"></i>
+                        </a>';
+                    }
+
+                    return '<div class="flex flex-col items-center gap-2">' . $icon . '<button type="button" data-fc-type="modal" data-fc-target="modalcenter"
+                        class="inline-block focus:outline-none text-slate-500 hover:bg-slate-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-slate-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-slate-500  text-sm font-medium py-1 px-3 rounded" onclick="openModal(' . $row->id . ')">
+                        Upload Receipt
+                    </button>' . '</div>';
+                }
+
+                return '';
             })
             ->rawColumns(['receipt'])
             ->toJson();
@@ -61,5 +81,19 @@ class TransactionController extends Controller
         ]);
 
         return redirect()->back();
+    }
+
+    public function completeTransaction(Request $request)
+    {
+        try {
+            $transaction = Transaction::find($request->transaction_id);
+            $transaction->update([
+                'status' => 'complete'
+            ]);
+
+            return redirect()->back()->with('success', "Transaction completed");
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', $th->getMessage());
+        }
     }
 }
