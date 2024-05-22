1d8e2f641b88772379a4ee17cca5965c9a3c47ee
mcsans <mihsansaepulloh9@gmail.com>
2024-05-22 21:05:08 +0700

    Add: modal review, review product and  review transaction

--- app/Http/Controllers/Customer/TransactionController.php
+++ app/Http/Controllers/Customer/TransactionController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\DetailTransaction;
 use App\Models\Transaction;
+use App\Models\User;
 use App\Service\UploadFileService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -59,7 +61,10 @@ class TransactionController extends Controller
                     </button>' . '</div>';
                 }
 
-                return '';
+                return '<div class="flex flex-col items-center gap-2">' . $icon . '<button type="button" data-fc-type="modal" data-fc-target="modal-review"
+                        class="inline-block focus:outline-none text-slate-500 hover:bg-slate-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-slate-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-slate-500  text-sm font-medium py-1 px-3 rounded" onclick="openModalReview(' . $row->id . ')">
+                        Review
+                    </button>' . '</div>';
             })
             ->rawColumns(['receipt'])
             ->toJson();
@@ -95,5 +100,25 @@ class TransactionController extends Controller
         } catch (\Throwable $th) {
             return redirect()->back()->with('error', $th->getMessage());
         }
+    }
+
+    public function getDetailProductTransaction(Request $request)
+    {
+        $products = DetailTransaction::select('products.id', 'products.name', 'products.image')->leftJoin('products', 'products.id', '=', 'detail_transactions.product_id')->where('transaction_id', $request->transaction_id)->get();
+
+        return response()->json(['data' => $products], 200);
+    }
+
+    public function reviewTransaction(Request $request)
+    {
+        $products = DetailTransaction::where('transaction_id', $request->transaction_id_review)->get();
+
+        foreach ($products as $product) {
+            if ($request->input("rating-" . $product->id) == null) {
+                $request->request->add(['rating-' . $product->id => '5']);
+            }
+        }
+
+        dd($request->all());
     }
 }
