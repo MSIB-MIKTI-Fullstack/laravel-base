d4f92944b465533a947da6d7636a336eed91dbd4
mcsans <mihsansaepulloh9@gmail.com>
2024-05-22 11:01:22 +0700

    Style: button transaction process & input label receipt number

--- resources/views/admin/transaction/index.blade.php
+++ resources/views/admin/transaction/index.blade.php
@@ -76,7 +76,7 @@
                             class="inline-block focus:outline-none text-red-500 hover:bg-red-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-red-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-red-500  text-sm font-medium py-1 px-3 rounded mr-1 close"
                             data-fc-dismiss>Close</button>
                         <button type="submit"
-                            class="inline-block focus:outline-none text-primary-500 hover:bg-primary-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-primary-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-primary-500  text-sm font-medium py-1 px-3 rounded">Yes</button>
+                            class="inline-block focus:outline-none text-primary-500 hover:bg-primary-500 hover:text-white bg-transparent border border-gray-200 dark:bg-transparent dark:text-primary-500 dark:hover:text-white dark:border-gray-700 dark:hover:bg-primary-500  text-sm font-medium py-1 px-3 rounded">Process</button>
                     </div>
                 </div>
             </form>
@@ -92,7 +92,7 @@
         $('#switch-valid').change(function() {
             if ($(this).is(':checked')) {
                 $('#receipt-number-input').html(`
-                <label for="receipt_number" class="font-medium text-sm text-slate-600 dark:text-slate-400">Receipt Name</label>
+                <label for="receipt_number" class="font-medium text-sm text-slate-600 dark:text-slate-400">Receipt Number</label>
                 <input type="text" id="receipt_number" name="receipt_number"
                     class="form-input w-full rounded-md mt-1 border border-slate-300/60 dark:border-slate-700 dark:text-slate-300 bg-transparent px-3 py-1 focus:outline-none focus:ring-0 placeholder:text-slate-400/70 placeholder:font-normal placeholder:text-sm hover:border-slate-400 focus:border-primary-500 dark:focus:border-primary-500  dark:hover:border-slate-700"
                     required="">
