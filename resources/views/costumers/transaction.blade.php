bc85137a953a4ce5d8042348c41f885b86c1f4ed
MohammadIndraA <akunpertamasaya5@gmail.com>
2024-05-22 19:45:41 +0700

    update : transaction complate status customer

--- resources/views/costumers/transaction.blade.php
+++ resources/views/costumers/transaction.blade.php
@@ -1,6 +1,18 @@
 <x-customer-layout>
-    <div class="page-wrapper relative  duration-300 pt-0 w-full">
-        <div class="xl:w-full  min-h-[calc(100vh-56px)] relative pb-0">
+    <div class="xl:w-full  min-h-[calc(100vh-56px)] relative pb-0">
+        @if (\Session::has('success'))
+            <div class="p-3 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800"
+                role="alert">
+                {!! \Session::get('success') !!}
+            </div>
+        @endif
+        @if (\Session::has('error'))
+            <div class="p-3 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800"
+                role="alert">
+                {!! \Session::get('error') !!}
+            </div>
+        @endif
+        <div class="page-wrapper relative  duration-300 pt-0 w-full">
             <div class="container my-4 bg-white">
                 <div class="grid grid-cols-12 sm:grid-cols-12 md:grid-cols-12 lg:grid-cols-12 xl:grid-cols-12 gap-4">
                     <div class="col-span-12 sm:col-span-12 md:col-span-12 lg:col-span-12 xl:col-span-12">
@@ -91,7 +103,7 @@
                             return `<span class="bg-yellow-500 text-white text-[11px] font-medium mr-1 px-2.5 py-0.5 rounded-full ">Process</span>`
                         }
                         if (data == "delivery") {
-                            return `<span class="bg-orange-500 text-white text-[11px] font-medium mr-1 px-2.5 py-0.5 rounded-full ">Complete</span>`
+                            return `<span class="bg-orange-500 text-white text-[11px] font-medium mr-1 px-2.5 py-0.5 rounded-full ">Delivery</span>`
                         }
                         if (data == "reject") {
                             return `<span class="bg-red-500 text-white text-[11px] font-medium mr-1 px-2.5 py-0.5 rounded-full ">Reject</span>`
