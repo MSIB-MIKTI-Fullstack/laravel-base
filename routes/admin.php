a15ac37ba6cd41bee112ce8689e260fe13791450
DewaAyuu <dwyudiah02@gmail.com>
2024-05-22 20:32:25 +0800

    add: transaction process

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\ProductCategoryController;
 use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\TransactionController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,9 @@ Route::group(['as' => 'admin.'], function () {
         'product' => ProductController::class,
         'product-categories' => ProductCategoryController::class, 'users' => UserController::class,
     ]);
+
+    Route::group(['prefix' => '/transactions', 'as' => 'transactions.'], function () {
+        Route::get('/', [TransactionController::class, 'index'])->name('index');
+        Route::post('/process-transaction', [TransactionController::class, 'processTransaction'])->name('process-transaction');
+    });
 });
