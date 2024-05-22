7f9b6337dae2da3b5f52986d4dca7b0eb819d1ec
rrnaldi <renaldi240402@gmail.com>
2024-05-22 19:45:30 +0700

    update: transaction complete customer

--- resources/views/customers/transaction.blade.php
+++ resources/views/customers/transaction.blade.php
@@ -1,4 +1,14 @@
 <x-customer-layout>
+    @if (\Session::has('success'))
+    <div class="p-3 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800" role="alert">
+        {!! \Session::get('success') !!}
+    </div>
+    @endif
+    @if (\Session::has('error'))
+    <div class="p-3 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+        {!! \Session::get('error') !!}
+    </div>
+    @endif
     <div class="page-wrapper relative  duration-300 pt-0 w-full">
         <div class="xl:w-full  min-h-[calc(100vh-56px)] relative pb-0">
             <div class="container my-4 bg-white">
