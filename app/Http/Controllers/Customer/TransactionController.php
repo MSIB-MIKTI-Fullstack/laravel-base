210015b19f6440c81521aa094961f8decd61bcad
LinggaS <ucihalingga12@gmail.com>
2024-05-22 18:03:40 -0400

    add: review transaction

--- app/Http/Controllers/Customer/TransactionController.php
+++ app/Http/Controllers/Customer/TransactionController.php
@@ -105,4 +105,16 @@ class TransactionController extends Controller
 
         return response()->json(['data' => $products], 200);
     }
+    public function reviewTransaction(Request $request)
+    {
+        $products = DetailTransaction::where('transaction_id', $request->transaction_id_review)->get();
+
+        foreach ($products as $product) {
+            if ($request->input("rating-" . $product->id) == null) {
+                $request->request->add(['rating-' . $product->id => '5']);
+            }
+        }
+
+        dd($request->all());
+    }
 }
