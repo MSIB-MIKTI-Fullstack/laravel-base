72b976d78c295f96658ea85d269c2dc221b53430
Fadjaria <202110715203@mhs.ubharajaya.ac.id>
2024-05-22 16:34:21 +0700

    fix: bug sidenav

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -116,6 +116,12 @@
             } else {
                 appendScript()
             }
+
+            reinitializeSidenav()
+        }
+
+        function reinitializeSidenav() {
+            $('#sidenav-list').html($('#sidenav-list').html())
         }
 
         function number_format(number) {
