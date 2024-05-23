e64405dc388c2ffdf06a90f2c864b107f60fb89a
Velavor <naufalnarukami548@gmail.com>
2024-05-23 19:53:35 +0700

    add: review process transaction

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,4 +15,8 @@ class Product extends Model
     {
         return $this->belongsTo(ProductCategory::class);
     }
+    public function rating()
+    {
+        return $this->hasMany(Rating::class);
+    }
 }
