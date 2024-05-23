0c90d58e70aa618c9ac17ca2e111f29059e2650a
Raja Hariadi <rajahariadi0@gmail.com>
2024-05-23 19:40:43 +0700

    add: rating model & migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
@@ -22,35 +22,36 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            ProductCategorySeeder::class,
-            ProductSeeder::class,
-            CartSeeder::class
+            // ProductCategorySeeder::class,
+            // ProductSeeder::class,
+            // CartSeeder::class
+            RatingSeeder::class
         ]);
 
-        \App\Models\User::create([
-            'name' => 'Raja Hariadi',
-            'email' => 'rajahariadi0@gmail.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('qwerty123'), // password
-            'two_factor_secret' => null,
-            'two_factor_recovery_codes' => null,
-            'remember_token' => Str::random(10),
-            'profile_photo_path' => null,
-            'role' => 'user',
-            'current_team_id' => null,
-        ]);
+        // \App\Models\User::create([
+        //     'name' => 'Raja Hariadi',
+        //     'email' => 'rajahariadi0@gmail.com',
+        //     'email_verified_at' => now(),
+        //     'password' => Hash::make('qwerty123'), // password
+        //     'two_factor_secret' => null,
+        //     'two_factor_recovery_codes' => null,
+        //     'remember_token' => Str::random(10),
+        //     'profile_photo_path' => null,
+        //     'role' => 'user',
+        //     'current_team_id' => null,
+        // ]);
 
-        \App\Models\User::create([
-            'name' => 'Administrator',
-            'email' => 'admin@gmail.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('qwerty123'), // password
-            'two_factor_secret' => null,
-            'two_factor_recovery_codes' => null,
-            'remember_token' => Str::random(10),
-            'profile_photo_path' => null,
-            'role' => 'admin',
-            'current_team_id' => null,
-        ]);
+        // \App\Models\User::create([
+        //     'name' => 'Administrator',
+        //     'email' => 'admin@gmail.com',
+        //     'email_verified_at' => now(),
+        //     'password' => Hash::make('qwerty123'), // password
+        //     'two_factor_secret' => null,
+        //     'two_factor_recovery_codes' => null,
+        //     'remember_token' => Str::random(10),
+        //     'profile_photo_path' => null,
+        //     'role' => 'admin',
+        //     'current_team_id' => null,
+        // ]);
     }
 }
